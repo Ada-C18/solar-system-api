4d70a4fae225d6c564ae5809d614fcb6062ce311
Svetlana <svetikyss89@gmail.com>
2022-10-21 11:17:51 -0700

    solar_system

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,37 @@
-from flask import Blueprint
+from flask import Blueprint,jsonify
 
+class Planet:
+    def __init__(self,id,name,eq_diam,mass,moons,description):
+        self.id = id
+        self.name = name
+        self.eq_diam = eq_diam
+        self.mass = mass
+        self.moons = moons
+        self.description = description
+    
+        
+
+PLANETS =[
+    
+    Planet(1, "Mercury",0.383,0.06,0,"the smallest planet in our solar system and closest to the Sun—is only slightly larger than Earth's Moon. Mercury is the fastest planet, zipping around the Sun every 88 Earth days"),
+    Planet(2, "Venus",0.949,0.81,0,"Venus spins slowly in the opposite direction from most planets. A thick atmosphere traps heat in a runaway greenhouse effect, making it the hottest planet in our solar system"),
+    Planet(3, "Earth",1.000,1.00,1,"our home planet—is the only place we know of so far that’s inhabited by living things. It's also the only planet in our solar system with liquid water on the surface" )
+    ]        
+planets_bp = Blueprint('planets_bp', __name__, url_prefix='/planets')  
+
+@planets_bp.route('',methods=['GET'])
+def get_all_planets():
+    planet_response = []
+    for planet in PLANETS:
+        planet_response.append({
+            'id': planet.id,
+            'name': planet.name,
+            'eq_diam': planet.eq_diam,
+            'mass': planet.mass,
+            'moons': planet.moons,
+            'deskriptions': planet.description
+
+        })
+    print(type(planet_response))
+    print(type(jsonify(planet_response)))
+    return  jsonify(planet_response) 
