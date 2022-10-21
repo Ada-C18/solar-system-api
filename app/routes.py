3d27d292670b0831593a691c3e47d5ba57598d20
Lindsey <lindseymarcea@icloud.com>
2022-10-21 13:54:53 -0400

    Changes to class

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,19 @@
 from flask import Blueprint
 
+class Planet():
+    def __init__(self, id, name, description, color):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.color = color
+
+Planet = [
+    Planet(1, "Saturn", "gassy giant", "orange"),
+    Planet(2, "Mercury", "small rocky", "gray"),
+    Planet(3, "Pluto", "still a planet", "white")
+]
+
+planet_bp = Blueprint("Planet", __name__, url_prefix="/planet")
+
+
+
