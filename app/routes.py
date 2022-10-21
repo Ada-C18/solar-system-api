905731f8d7040ac52c09d6fe66594eb6c9ea8a1c
Pavi <pavanirannulu@gmail.com>
2022-10-21 10:57:19 -0700

    Flask setup and defining planet class

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,15 @@
 from flask import Blueprint
 
+class Planet():
+    def __init__(self, id, name, description, moons):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.moons = moons
+
+list_of_planets = [
+    Planet(1, "Mercury", "Grey smallest planet",0),
+    Planet(2, "Venus", "Orange planet of love", 0),
+    Planet(3, "Earth", "Our planet!", 1),
+    Planet(4, "Mars", "Red planet that we're exploring", 2)
+]
