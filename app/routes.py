4f9545eecc6167f9dbd05cc46e6f463a7615231c
Bianca <biancadelascasas1@gmail.com>
2022-10-21 15:24:02 -0400

    set up planets endpoint to return planet descriptors

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,24 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
+
+class Planet:
+    def __init__(self, id, name, description, radius):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.radius = radius
+
+
+planets = [
+    Planet(1, 'Earth', 'good place', 6371 ), 
+    Planet(2, 'Jupiter', 'fun place', 69901),
+    Planet(3, 'Mars', 'weird place', 3389.5)
+]
+
+bp = Blueprint('planets', __name__, url_prefix='/planets')
+@bp.route('', methods=["GET"])
+def get_planets():
+    planet_data = []
+    for planet in planets:
+        planet_data.append(dict(id=planet.id, name=planet.name, description=planet.description, radius=planet.radius))
+    return jsonify(planet_data)
