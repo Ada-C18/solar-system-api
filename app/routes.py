20b220fb66388597d75082fd27dcd3397e6b2301
Yael <yaelechols@gmail.com>
2022-10-21 10:54:13 -0700

    Implements Planet class and route

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,34 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
+class Planet():
+    def __init__(self, id, name, description, moon_count):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.moon_count = moon_count
+
+PLANETS = [
+    Planet(1, "Mercury"),
+    Planet(2, "Venus"),
+    Planet(3, "Mars"),
+    Planet(4, "Earth"),
+    Planet(5, "Jupiter"),
+    Planet(6, "Saturn"),
+    Planet(7, "Uranus"),
+    Planet(8, "Neptune")
+]
+
+planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+@planets_bp.route("", methods=["GET"])
+def get_all_planets():
+    result = []
+    for planet in PLANETS:
+        result.append({
+            "id": planet.id,
+            "name": planet.name,
+            "description": planet.description,
+            "moon count": planet.moon_count
+        })
+
+    return jsonify(result)
