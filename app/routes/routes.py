d1bc0b11eeabb9b2fb7a6b6fa6545a61dc810d1d
Adelle Montalbo <adellemontalbo@gmail.com>
2022-10-21 11:17:26 -0700

    created new folder 'routes' created class 'Planet', created endpoint 'GET'

---
+++ app/routes/routes.py
@@ -0,0 +1,32 @@
+from flask import Blueprint, jsonify
+
+class Planet:
+    def __init__(self, id, name, description, size):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.size = size
+
+
+planets = [
+    Planet(1, "Saturn", "cold", "large"),
+    Planet(2, "Earth", "normal", "medium"),
+    Planet(3, "Jupiter", "sunny", "large")
+        ]
+
+bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+@bp.route("", methods=["GET"])
+def all_planets():
+    results_list = []
+    for planet in planets:
+        results_list.append(dict(
+            id=planet.id,
+            name=planet.name,
+            description=planet.description,
+            size=planet.size
+        ))
+    return jsonify(results_list)
+    
+
+
