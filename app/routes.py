ff392d58369648755735af313d6ea978f7b17052
Sunny <1sunnymuniz@gmail.com>
2022-10-21 12:02:39 -0600

    begun wave 1, have created one instance of planets

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,11 @@
 from flask import Blueprint
 
+
+class Planets(): 
+    def __init__(self, id, name, description, diameter): 
+        self.id = id
+        self.name = name 
+        self.description = description 
+        self.diameter = diameter 
+
+PLANETS = [Planets(1, "Earth", "round and trashy", "unknown")] 
