90963d7a01c34c96b2fa01345b4bbbe1123401d0
Anika Stephen Wilbur <anika.m.stephen@gmail.com>
2022-10-21 11:10:04 -0700

    Built out planets_endpoint

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
 class Planet:
     def __init__(self, id, name, description, moons):
@@ -18,4 +18,12 @@ planets_bp = Blueprint("planets", __name__, url_defaults="/planets")
 
 @planets_bp.route("", methods=["GET"])
 def planets_endpoint():
-    pass
+    response = []
+    for planet in planets:
+        response.append(dict(
+            id = planet.id,
+            name = planet.name,
+            description = planet.description,
+            moons = planet.moons
+        ))
+    return jsonify(response)
