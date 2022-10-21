8fe6e8dfb7371ccd613e3a6ab88c55e4016b8908
Geiselle Holt <geiselleholt@hotmail.com>
2022-10-21 12:20:41 -0600

    added additional attribute moons

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,16 @@
 from flask import Blueprint, jsonify
 
 class Planet:
-    def __init__(self, id, name, description):
+    def __init__(self, id, name, description, moons):
         self.id = id
         self.name = name
         self.description = description
+        self.moons = moons
 
 planets = [
-    Planet(1, "Mercury", "closest to the sun"),
-    Planet(2, "Venus", "second closest to the sun"),
-    Planet(3, "Earth", "where we live")
+    Planet(1, "Mercury", "closest to the sun", 0),
+    Planet(2, "Venus", "second closest to the sun", 0),
+    Planet(3, "Earth", "where we live", 1)
 ]
 
 bp = Blueprint("planets", __name__, url_prefix = "/planets")
@@ -22,7 +23,8 @@ def handle_planets():
         results_list.append(dict(
             id=planet.id,
             name=planet.name,
-            description=planet.description
+            description=planet.description,
+            moons=planet.moons
 
         ))
 
