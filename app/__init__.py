ad91652c165a93dee4ec3ecf85a3ba47371939f6
Niloofar Nayebi <nil_nyb@yahoo.com>
2022-10-21 18:07:10 -0400

    wave_1 in progress

--- app/__init__.py
+++ app/__init__.py
@@ -3,5 +3,9 @@ from flask import Flask
 
 def create_app(test_config=None):
     app = Flask(__name__)
+    
+    from .routes import  solar_system_bp
+    app.register_blueprint(solar_system_bp)
+    
 
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,29 @@
-from flask import Blueprint
+
+from flask import Blueprint , jsonify
+
+solar_system_bp = Blueprint("solar_system", __name__)
+
+class Planet:
+    def __init__(self , id , name, description, size):
+        self.id = id
+        self.name=name
+        self.description = description
+        self.size=size
+planets = [
+    Planet(1, "Fictional Book Title", "A fantasy novel set in an imaginary world.", 2),
+    Planet(2, "Fictional Book Title", "A fantasy novel set in an imaginary world.", 3),
+    Planet(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.", 4)
+]
+solar_system_bp=Blueprint("planets",__name__ ,  url_prefix="/planets") 
+@solar_system_bp.route("",methods=["GET"])  
+def handle_planet():
+    result=[]
+    for i in planets:
+        result.append({
+            "id":i.id,
+            "name":i.name,  
+            "description":i.description,
+            "size":i.size
+        })  
+    return jsonify(result) 
 
