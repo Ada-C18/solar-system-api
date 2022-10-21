3238a538948abd659770560605f466af305847fa
Elsje Slothower <elsje.slothower@gmail.com>
2022-10-21 11:24:33 -0700

    completed flask setup, created Planet class and instances

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,30 @@
 from flask import Blueprint
 
+class Planet:
+    def __init__(self, id, name, description):
+        self.id = id
+        self.name = name
+        self.description = description
+
+mercury = Planet(1, "Mercury", "smallest planet")
+venus = Planet(2, "Venus", "hottest planet")
+earth = Planet(3, "Earth", "only planet known to harbor intelligent life")
+mars = Planet(4, "Mars", "most likely planet to terraform")
+jupiter = Planet(5, "Jupiter", "largest planet")
+saturn = Planet(6, "Saturn", "only planet with a ring system")
+uranus = Planet(7, "Uranus", "only planet with an almost vertical equator")
+neptune = Planet(8, "Neptune", "coldest planet")
+pluto = Planet(9, "Pluto", "only planet to be disowned fromt the Solar System")
+
+solar_system = [
+    mercury,
+    venus,
+    earth,
+    mars,
+    jupiter,
+    saturn,
+    uranus,
+    neptune,
+    pluto
+]
+
