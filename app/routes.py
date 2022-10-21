73f1859868cc3cd8becfb89e6bb1eb9a83cac707
Kat <kmoreno1205@gmail.com>
2022-10-21 12:27:50 -0600

    Added all PLANETS data to the instance.

--- app/routes.py
+++ app/routes.py
@@ -8,4 +8,13 @@ class Planets():
         self.description = description 
         self.diameter = diameter 
 
-PLANETS = [Planets(1, "Earth", "round and trashy", "unknown")] 
+PLANETS = [
+    Planets(1, "Mercury", "closest planet to the sun", "3032 miles"), 
+    Planets(1, "Venus", "hottest planet", "7521 miles"), 
+    Planets(1, "Earth", "round and trashy", "7917 miles"),
+    Planets(1, "Mars", "reddish hue", "4212 miles"), 
+    Planets(1, "Jupiter", "largest planet", "86881 miles"), 
+    Planets(1, "Saturn", "surrounded by rings", "72367 miles"), 
+    Planets(1, "Uranus", "coldest planet", "31518 miles"), 
+    Planets(1, "Neptune", "most windy planet", "30599 miles"), 
+]
