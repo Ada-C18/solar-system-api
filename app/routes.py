eff763fe8d7bd036fa745f7cbfcf2e515ca07008
Kae Sluder <kaesluder@fastmail.com>
2022-10-21 20:39:13 -0400

    minor changes and a bug fix to send planet.name

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify
 
+
 class Planet:
     def __init__(self, id, name, description, distance):
         self.id = id
@@ -7,18 +8,26 @@ class Planet:
         self.description = description
         self.distance = distance
 
-Planet_list = [Planet(1, "Mercury", "tiny and hot", 300), Planet(2, "Venus", "big and hot", 600), Planet(3, "Earth", "home", 900)]
 
-bp = Blueprint("planets", __name__, url_prefix= "/planet")
+planet_list = [
+    Planet(1, "Mercury", "tiny and hot", 0.387),
+    Planet(2, "Venus", "big and hot", 0.722),
+    Planet(3, "Earth", "home", 1.0),
+]
+
+bp = Blueprint("planets", __name__, url_prefix="/planet")
+
 
 @bp.route("", methods=["GET"])
 def handle_planets():
     planets_list = []
-    for planet in Planet_list:
-        planets_list.append(dict(
-            id = planet.id, 
-            name = planet.id,
-            description = planet.description,
-            distance = planet.distance
-        ))
+    for planet in planet_list:
+        planets_list.append(
+            dict(
+                id=planet.id,
+                name=planet.name,
+                description=planet.description,
+                distance=planet.distance,
+            )
+        )
     return jsonify(planets_list)
