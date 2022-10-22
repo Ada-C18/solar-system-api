2db24fd92dda3035a29209cbe1b69de966965b0f
ashanti <ammanier2@gmail.com>
2022-10-22 14:11:27 -0400

    hardcoded planet instances

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,22 @@
 from flask import Blueprint, jsonify
 
 class Planet:
-    def __init__(self, id, name, description, moons):
+    def __init__(self, id, name, description, revolution_period):
        self.id = id
        self.name = name 
        self.description = description
-       self.moons = moons
+       self.revolution_period = revolution_period
+
+PLANETS = [
+    Planet(1, "Mercury", "terrestrial", "87.97 days"),
+    Planet(2, "Venus", "terrestrial", "224.7 days"),
+    Planet(3, "Earth", "terrestrial", "365.26 days"),
+    Planet(4, "Mars", "terrestrial", "1.88 years"),
+    Planet(5, "Jupiter", "gaseous", "11.86 years"),
+    Planet(6, "Saturn", "gaseous", "29.46 years"),
+    Planet(7, "Uranus", "gaseous", "84.01 years"),
+    Planet(8, "Neptune", "gaseous", "164.79 years"),
+    Planet(9, "Pluto", "icy, rocky", "248.59 years")
+]
+
 
