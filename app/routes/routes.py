5307378f75ae8556ab280e6da68330472e6c9d38
Jessica Hebert <jessicahebert75@gmail.com>
2022-10-24 12:21:03 -0700

    done test

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, jsonify
+# from os import abort
+from flask import Blueprint, jsonify, abort, make_response
 
 class Planet:
     def __init__(self, id, name, description, size):
@@ -7,6 +8,13 @@ class Planet:
         self.description = description
         self.size = size
 
+    def to_json(self):
+        return dict(
+            id=self.id,
+            name=self.name,
+            description=self.description,
+            size=self.size
+    )
 
 planets = [
     Planet(1, "Saturn", "cold", "large"),
@@ -26,6 +34,25 @@ def all_planets():
         ) for planet in planets]
 
     return jsonify(results_list)
-    
+
+def  validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except:
+        abort(make_response({"message": f"planet {planet_id} invalid"}, 404))
+
+    for planet in planets:
+        if planet.id == planet_id:
+            return planet
+    abort(make_response({"message": f"planet {planet_id} not found"}, 400))
+
+
+@bp.route("/<id>", methods = ["GET"])
+def handle_planet (id):
+    planet = validate_planet(id)
+    return jsonify(planet.json())
+
+
+
 
 
