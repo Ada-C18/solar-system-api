ca9e751963129a68ed91c68f5d97d12591fc875b
Pavi <pavanirannulu@gmail.com>
2022-10-24 11:37:58 -0700

    fixed issues

--- app/routes.py
+++ app/routes.py
@@ -11,12 +11,12 @@ list_of_planets = [
     Planet(1, "Mercury", "Grey smallest planet",0),
     Planet(2, "Venus", "Orange planet of love", 0),
     Planet(3, "Earth", "Our planet!", 1),
-    Planet(4, "Mars", "Red planet that we're exploring", 2)
-    Planet(5, "Jupiter",  "Biggest planet in the solar system", 80)
-    Planet(6, "Saturn", "The planet with the rings", 83)
-    Planet(7, "Uranus", "Ice giant", 27)
-    Planet(8, "Neptune", "A cold blue planet", 14)
-    Planet(9, "Pluto", "The tiniest dwarf planet", 5)
+    Planet(4, "Mars", "Red planet that we're exploring", 2),
+    Planet(5, "Jupiter",  "Biggest planet in the solar system", 80),
+    Planet(6, "Saturn", "The planet with the rings", 83),
+    Planet(7, "Uranus", "Ice giant", 27),
+    Planet(8, "Neptune", "A cold blue planet", 14),
+    Planet(9, "Pluto", "The tiniest dwarf planet", 5),
 ]
 
 planets_bp = Blueprint("planets", __name__)
