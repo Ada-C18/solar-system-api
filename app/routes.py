a6d33da2cc056bcef23e0d0c8c7c7827a9ce78e4
Elsje Slothower <elsje.slothower@gmail.com>
2022-10-24 10:33:18 -0700

    setup bp route, registered bp with app

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, abort, make_response
 
 class Planet:
     def __init__(self, id, name, description):
@@ -6,25 +6,56 @@ class Planet:
         self.name = name
         self.description = description
 
-mercury = Planet(1, "Mercury", "smallest planet")
-venus = Planet(2, "Venus", "hottest planet")
-earth = Planet(3, "Earth", "only planet known to harbor intelligent life")
-mars = Planet(4, "Mars", "most likely planet to terraform")
-jupiter = Planet(5, "Jupiter", "largest planet")
-saturn = Planet(6, "Saturn", "only planet with a ring system")
-uranus = Planet(7, "Uranus", "only planet with an almost vertical equator")
-neptune = Planet(8, "Neptune", "coldest planet")
-pluto = Planet(9, "Pluto", "only planet to be disowned fromt the Solar System")
-
 solar_system = [
-    mercury,
-    venus,
-    earth,
-    mars,
-    jupiter,
-    saturn,
-    uranus,
-    neptune,
-    pluto
+    Planet(1, "Mercury", "smallest planet"),
+    Planet(2, "Venus", "hottest planet"),
+    Planet(3, "Earth", "only planet known to harbor intelligent life"),
+    Planet(4, "Mars", "most likely planet to terraform"),
+    Planet(5, "Jupiter", "largest planet"),
+    Planet(6, "Saturn", "only planet with a ring system"),
+    Planet(7, "Uranus", "only planet with an almost vertical equator"),
+    Planet(8, "Neptune", "coldest planet"),
+    Planet(9, "Pluto", "only planet to be disowned fromt the Solar System")
 ]
+
+solar_system_bp = Blueprint(
+    "solar_system_bp", __name__, url_prefix="/solar-system"
+    )
+
+@solar_system_bp.route("", methods=["GET"])
+def handle_planets():
+    planets_response = []
+    for planet in solar_system:
+        planets_response.append(
+            {
+                "id": planet.id,
+                "name": planet.name,
+                "description": planet.description
+            }
+        )
+    return jsonify(planets_response)
+
+@solar_system_bp.route("/<planet_id>", methods=["GET"])
+def handle_planet(planet_id):
+    planet = verify_planet(planet_id)
+
+    return {
+        "id": planet.id,
+        "name": planet.name,
+        "description": planet.description,
+    }
+
+def verify_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except:
+        abort(make_response({"message":f"planet {planet_id} invalid"}, 400))
+    
+    for planet in solar_system:
+        if planet.id == planet_id:
+            return planet
+    
+    return abort(make_response(
+        {"message":f"planet {planet_id} not found"}, 404
+        ))
 
