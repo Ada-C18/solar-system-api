79db9c62349034e1263d814654f17d07547ab158
Reyna Diaz <reynamdiazz@gmail.com>
2022-10-24 14:47:30 -0400

    "adds wave 01 class + list"

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,15 @@
 from flask import Blueprint
 
+class Planet:
+
+    def __init__(self, id, name, description, moons):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.moons = moons
+
+planets = [
+    Planet(1, "Earth", "our home", 1),
+    Planet(2, "Mars", "red planet", 2),
+    Planet(3, "Pluto", "is a planet", 5)
+]
