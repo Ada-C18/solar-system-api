799e620aa2bfadd4ed228c26e9b5d2c3bcd4a1e2
Annie <annie.gallagher7@gmail.com>
2022-10-24 12:38:59 -0600

    added single planet endpoint

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 class Planet:
     def __init__(self, id, name, description, moons):
@@ -7,6 +7,12 @@ class Planet:
         self.description = description
         self.moons = moons
 
+    def to_json(self):
+        dict(id = self.id,
+            name = self.name,
+            description = self.description,
+            moons = self.moons
+            )
 
 planets = [
     Planet(1, "Mercury", "solid", 0),
@@ -18,9 +24,23 @@ planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
 @planets_bp.route("", methods=["GET"])
 def planets_endpoint():
-    response = [dict(id = planet.id,
-            name = planet.name,
-            description = planet.description,
-            moons = planet.moons
-            ) for planet in planets]
+    response = [planet.to_json for planet in planets]
     return jsonify(response)
+
+@planets_bp.route("/<planet_id>", methods=["GET"])
+def planet_endpoint(planet_id):
+    planet = validate_planet(planet_id)
+
+    return jsonify(planet.to_json)
+
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except:
+        abort(make_response({"message":f"planet {planet_id} invalid"}, 400))
+    
+    for planet in planets:
+        if planet.id == planet_id:
+            return planet
+    
+    abort(make_response({"message":f"planet {planet_id} not found"}, 404))
