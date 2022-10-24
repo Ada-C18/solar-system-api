eabcc72be7b08a4a8b974cb1aa746eb5dad47851
Katherine Abrikian <kabrikian@gmail.com>
2022-10-24 14:56:54 -0400

    wave 2 complete

--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,14 @@ class Planet():
         self.description = description
         self.rings = rings
 
+    def to_json(self):
+        return dict(
+            id = self.id,
+            name = self.name,
+            description = self.description,
+            rings = self.rings)
+
+
 earth = Planet(3, "Earth", "Blue planet", False)
 saturn = Planet(6, "Saturn", "Yellow", True)
 
@@ -18,9 +26,19 @@ bp = Blueprint("planets", __name__, url_prefix="/planets")
 def handle_planets():
     planet_list = []
     for planet in planets:
-        planet_list.append(dict(
-            id = planet.id,
-            name = planet.name,
-            description = planet.description,
-            rings = planet.rings))
+        planet_list.append(planet.to_json())
     return jsonify(planet_list)
+
+@bp.route("/<planet_id>", methods = ["GET"])
+def handle_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+
+    except:
+        return {"message":f"planet {planet_id} invalid"}, 400
+
+    for planet in planets:
+        if planet.id == planet_id:
+            return jsonify(planet.to_json())
+        
+    return {"message":f"planet {planet_id} not found"}, 404
