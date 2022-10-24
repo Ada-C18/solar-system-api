2f51a675652064a5958ec88b38f6cdf5a43fd410
Rahel Kemal <rahelskemal@gmail.com>
2022-10-24 11:56:18 -0700

    wave 2, validate planet

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,11 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 class Planet():
-    def __init__(self, id, name, description, moon_count):
+    def __init__(self, id, name):#removed description and moon_count for now
         self.id = id
         self.name = name
-        self.description = description
-        self.moon_count = moon_count
+        # self.description = description
+        # self.moon_count = moon_count
 
 PLANETS = [
     Planet(1, "Mercury"),
@@ -24,3 +24,19 @@ planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 def get_all_planets():
     result = [vars(planet) for planet in PLANETS]
     return jsonify(result)
+
+def validate_planet(planet_id): 
+    try:
+        planet_id = int(planet_id)
+    except:
+        abort(make_response({"message": f"{planet_id} is invalid"}, 400))
+    
+    for planet in PLANETS:
+        if planet.id == planet_id:
+            return vars(planet)
+    abort(make_response({"message": f"{planet_id} not found"}, 404))
+
+@planets_bp.route("/<planet_id>", methods=["GET"])
+def get_one_planet(planet_id):
+    planet = validate_planet(planet_id)
+    return planet
