ad18f89b23ca6a3248c95a891a62e2c1ce07c04f
Isabella <isabella.vanstedum@gmail.com>
2022-10-24 15:50:21 -0300

    completed wave 2

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 
 class Planet:
@@ -7,7 +7,8 @@ class Planet:
         self.name = name
         self.description = description
         self.radius = radius
-
+    def retrieve_planet(self):
+        return dict(id=self.id, name=self.name, description=self.description, radius=self.radius)
 
 planets = [
     Planet(1, 'Earth', 'good place', 6371 ), 
@@ -20,5 +21,19 @@ bp = Blueprint('planets', __name__, url_prefix='/planets')
 def get_planets():
     planet_data = []
     for planet in planets:
-        planet_data.append(dict(id=planet.id, name=planet.name, description=planet.description, radius=planet.radius))
+        planet_data.append(planet.retrieve_planet())
     return jsonify(planet_data)
+def validate_planet(id):
+    try:
+        planet_id = int(id)
+    except:
+        abort(make_response({"message": f"{id} is invalid"}, 400))
+    for planet in planets:
+        if planet_id == planet.id:
+            return(planet)
+    abort(make_response({"message": f"{id} not found"}, 404))
+@bp.route('<id>', methods=["GET"])
+def get_planet(id):
+    planet=validate_planet(id)
+    return planet.retrieve_planet()
+    
