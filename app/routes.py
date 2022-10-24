f9fcceed9d62335b92e16543e0ae0c40b53928b3
Lisa <elizabethpham726@gmail.com>
2022-10-24 11:54:22 -0700

    finishes wave 2 and adds helper functions

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 class Planet:
     def __init__(self, id, name, description, color):
@@ -15,15 +15,39 @@ PLANETS = [
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
+# HELPER FUNCTIONS
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except:
+        abort(make_response({"message": f"Planet {planet_id} invalid"}, 400))
+
+    for planet in PLANETS:
+        if planet.id == planet_id:
+            return planet
+
+    abort(make_response({"message": f"Planet {planet_id} not found"}, 404))
+
+def planet_dict(planet):
+    return {
+        "id": planet.id, 
+        "name": planet.name, 
+        "description": planet.description,
+        "color": planet.color
+    }
+
+# ROUTES
 @planets_bp.route("", methods=["GET"])
 def get_all_planets():
     planets_response = []
+    
     for planet in PLANETS:
-        planets_response.append({
-                                "id": planet.id, 
-                                "name": planet.name, 
-                                "description": planet.description,
-                                "color": planet.color
-                            })
+        planets_response.append(planet_dict(planet))
 
     return jsonify(planets_response)
+
+@planets_bp.route("/<planet_id>", methods=["GET"])
+def get_one_planet(planet_id):
+    planet = validate_planet(planet_id)
+    
+    return planet_dict(planet)
