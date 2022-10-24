cc4a0489be4532efd510d32ac546b4c0936e7eb4
allcomputersarebad <allcomputersarebad@users.noreply.github.com>
2022-10-24 11:49:44 -0700

    we did wave 2
    Co-authored-by: Paje-Bishop <Paje-Bishop@users.noreply.github.com>

--- app/routes.py
+++ app/routes.py
@@ -7,10 +7,18 @@ class Planet:
         self.name = name
         self.description = description
         self.mass = mass
+    
+    def planet_dict(self):
+        return self.id, {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "mass": self.mass
+        }
 
 
 planets = [
-    "Sol",
+    #"Sol",
     Planet(1, "Mercury", "just a little guy", "small"),
     Planet(2, "Venus", "what a babe", "hot"),
     Planet(3, "Earth", "nice planet", "normal"),
@@ -25,11 +33,18 @@ planet_bp = Blueprint("planet", __name__, url_prefix="/planets")
 
 
 @planet_bp.route("", methods=["GET"])
-def handle_planets():
-    return "Planets", 201
+def handle_all_planets():
+    all_planets = { p.id: p.planet_dict() for p in planets}
+    return all_planets, 200
 
 
 @planet_bp.route("/<planet_id>", methods=["GET"])
 def handle_planet(planet_id):
-    planet = planets[int(planet_id)]
-    return f"Planet {planet.id}, {planet.name}, {planet.description}, {planet.mass}", 201
+    try:
+        planet = planets[int(planet_id)-1]
+    except ValueError:
+        return {"error": "Invalid planet id"}, 400
+    except IndexError:
+        return {"error": "No planet by that id"}, 404
+    #return f"Planet {planet.id}, {planet.name}, {planet.description}, {planet.mass}", 200
+    return planet.planet_dict(), 200
