a7e88cedf8805951a01b74d993d0851d94be8c42
Geiselle Holt <geiselleholt@hotmail.com>
2022-10-25 12:24:07 -0600

    Added helper function for return dict

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,21 @@
 from flask import Blueprint, jsonify, abort, make_response
 
+
 class Planet:
     def __init__(self, id, name, description, moons):
         self.id = id
         self.name = name
         self.description = description
         self.moons = moons
+
+    def to_planet_dict(self):
+        return dict(
+            id=self.id,
+            name=self.name,
+            description=self.description,
+            moons=self.moons
+    )
+
 
 planets = [
     Planet(1, "Mercury", "closest to the sun", 0),
@@ -17,11 +27,11 @@ def validate_planet(planet_id):
     try:
         planet_id = int(planet_id)
     except:
-       abort(make_response({"message":f"planet {planet_id} invalid"}, 400))
+        abort(make_response({"message":f"planet {planet_id} invalid"}, 400))
 
     for planet in planets:
-       if planet.id == planet_id:
-           return planet
+        if planet.id == planet_id:
+            return planet
 
     abort(make_response({"message":f"planet {planet_id} not found"}, 404))
 
@@ -32,12 +42,7 @@ def handle_planets():
     results_list = []
 
     for planet in planets:
-        results_list.append(dict(
-            id=planet.id,
-            name=planet.name,
-            description=planet.description,
-            moons=planet.moons
-        ))
+        results_list.append(planet.to_planet_dict())
         
     return jsonify(results_list)
 
@@ -45,9 +50,4 @@ def handle_planets():
 def handle_one_planet(planet_id):
     planet = validate_planet(planet_id)
     
-    return dict(
-        id=planet.id,
-        name=planet.name,
-        description=planet.description,
-        moons=planet.moons
-    )
+    return jsonify(planet.to_planet_dict())
