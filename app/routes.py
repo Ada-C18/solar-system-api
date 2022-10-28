4c27e18f2ce962480cb12af255edc6bace92fe0c
Katherine Abrikian <kabrikian@gmail.com>
2022-10-28 14:55:36 -0400

    created routes to handle creating planet and get all

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, jsonify, abort, make_response
+from app import db
+from flask import Blueprint, jsonify, abort, make_response, request
 from .models.planet import Planet
 
 bp = Blueprint("planets", __name__, url_prefix="/planets")
@@ -10,6 +11,7 @@ def validate_planet(planet_id):
     except:
         abort(make_response({"message": f"planet {planet_id} invalid"}, 400))
 
+    planets = Planet.query.all()
     for planet in planets:
         if planet.id == planet_id:
             return planet
@@ -17,12 +19,27 @@ def validate_planet(planet_id):
     abort(make_response({"message": f"planet {planet_id} not found"}, 404))
 
 
-@bp.route("", methods=["GET"])
+@bp.route("", methods=["GET", "POST"])
 def handle_planets():
-    planet_list = []
-    for planet in planets:
-        planet_list.append(planet.to_dict())
-    return jsonify(planet_list)
+    if request.method == "GET":
+        planets = Planet.query.all()
+        planets_response = []
+        for planet in planets:
+            planets_response.append(planet.to_dict())
+        return jsonify(planets_response)
+    elif request.method == "POST":
+        request_body = request.get_json()
+        new_planet = Planet(
+            # id=request_body["id"],
+            name=request_body["name"], 
+            description=request_body["description"],
+            rings=request_body["rings"]
+        )
+
+        db.session.add(new_planet)
+        db.session.commit()
+
+        return make_response(f"Planet {new_planet.name} successfully created", 201)
 
 
 @bp.route("/<planet_id>", methods=["GET"])
