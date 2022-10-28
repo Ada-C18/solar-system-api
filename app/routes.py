85bbcade6553fadf860eb5bb66deb60700b536d2
Marie Keefer <mkeefer17@gmail.com>
2022-10-28 15:16:51 -0400

    working on wave 3 restful endpoints

--- app/routes.py
+++ app/routes.py
@@ -1,32 +1,35 @@
-from flask import Blueprint, jsonify, abort, make_response
+from app import db
+from app.models.planet import Planet
+from flask import Blueprint, jsonify, abort, make_response, request
 
-class Planet:
-    def __init__(self, id, name, description, size):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.size = size
-planets = [
-    Planet(1, "Mars", "red", "small"),
-    Planet(2, "Venus", "poisonous", "smallish"),
-    Planet(3, "Saturn", "has rings", "large")
-]
+# class Planet:
+#     def __init__(self, id, name, description, size):
+#         self.id = id
+#         self.name = name
+#         self.description = description
+#         self.size = size
+# planets = [
+#     Planet(1, "Mars", "red", "small"),
+#     Planet(2, "Venus", "poisonous", "smallish"),
+#     Planet(3, "Saturn", "has rings", "large")
+# ]
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
-def validate(planet_id):
-    try:
-        planet_id = int(planet_id)
-    except:
-        abort(make_response({"message": f"Invalid planet id: '{planet_id}'"}, 400))
-    planet = Planet.query.get(planet_id)
-    if planet:
-        return planet
+# def validate(planet_id):
+#     try:
+#         planet_id = int(planet_id)
+#     except:
+#         abort(make_response({"message": f"Invalid planet id: '{planet_id}'"}, 400))
+#     planet = Planet.query.get(planet_id)
+#     if planet:
+#         return planet
 
-    abort(make_response({"message": f'planet id {planet_id} not found'}, 404))
+#     abort(make_response({"message": f'planet id {planet_id} not found'}, 404))
 
 @planets_bp.route("", methods=["GET"])
 def handle_planets():
+    planets = Planet.query.all()
     planets_response = []
     for planet in planets:
         planets_response.append({
