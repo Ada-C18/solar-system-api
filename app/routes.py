3a8e712183eb564bad1b71bf96242b864055f0f5
Luciane Dias <lucilewa@yahoo.com>
2022-10-28 13:52:52 -0700

    uptadet migration

--- app/routes.py
+++ app/routes.py
@@ -28,7 +28,8 @@ planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 #     abort(make_response({"message": f'planet id {planet_id} not found'}, 404))
 
 @planets_bp.route("", methods=["GET"])
-def handle_planets():
+def read_all_planets():
+    request.method == "GET"
     planets = Planet.query.all()
     planets_response = []
     for planet in planets:
@@ -40,3 +41,17 @@ def handle_planets():
         })
 
     return jsonify(planets_response)
+
+@planets_bp.route("", methods=["POST"])
+def create_planet():
+    request_body = request.get_json()
+    new_planet = Planet(
+        name=request_body["name"], 
+        description=request_body["description"],
+        size=request_body["size"]
+    )
+    db.session.add(new_planet)
+    db.session.commit()
+
+    return make_response(f"Planet {new_planet.name} created", 201)
+    
