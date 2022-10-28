1ece771f0b1badf194614a34ce9e205832945bc9
reneesu99 <reneesu22@gmail.com>
2022-10-28 12:17:36 -0700

    added get method to planets

--- app/routes.py
+++ app/routes.py
@@ -1,26 +1,32 @@
+from time import monotonic_ns
 from app import db
 from app.models.planet import Planet
 from flask import Blueprint, jsonify, make_response, request
 
 planet_bp = Blueprint("planet_bp", __name__, url_prefix="/planets")
 
-@planet_bp.route("", methods=["POST"])
+@planet_bp.route("", methods=["POST", "GET"])
 def handle_planets():
-    request_body = request.get_json()
-    planet_1 = Planet(
-        name = request_body["name"], 
-        description = request_body["description"],
-        moons = request_body["moons"])
+    if request.method == "POST":
+        request_body = request.get_json()
+        planet_1 = Planet(
+            name = request_body["name"], 
+            description = request_body["description"],
+            moons = request_body["moons"])
+        db.session.add(planet_1)
+        db.session.commit()
 
-    db.session.add(planet_1)
-    db.session.commit()
-
-    return make_response(f"Planet {planet_1.name} successfully created", 201)
-
-# @planet_bp.route("", methods = ["GET"])
-# def read_planets():
-#     return return_all_planets()
-
-# @planet_bp.route("/<planet_id>", methods=["GET"])
-# def handle_planet(planet_id):
-#     return verify_planet(planet_id) 
+        return make_response(f"Planet {planet_1.name} successfully created", 201)
+    
+    if request.method == "GET":
+        response_body = []
+        planets= Planet.query.all()
+        for planet in planets:
+            response_body.append({
+                "name": planet.name,
+                "description": planet.description,
+                "moons": planet.moons,
+                "id": planet.id,
+            })
+            
+        return jsonify(response_body), 200
