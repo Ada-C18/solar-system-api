e70f4473d43b67d5bf132c227b35cb13b9a37250
Anaiq <>
2022-10-27 21:36:33 -0500

    donfigured db in __init__.py, created routes to add planet and get all planets in route.py, created Planet model

--- app/routes.py
+++ app/routes.py
@@ -1,69 +1,35 @@
-from flask import Blueprint, jsonify, abort, make_response
+from app import db
+from app.models.planet import Planet
+from flask import Blueprint, jsonify, make_response, request
 
-"""
-1. Define a Planet class with the attributes id, name, and description, 
-and one additional attribute
-2. Create the following endpoint(s), with similar functionality presented in the
-Hello Books API.
-"""
+planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
+@planets_bp.route("", methods=["POST"])
+def create_planet():
+    request_body = request.get_json()
+    new_planet = Planet(name=request_body["name"],
+                    surface_area=request_body["surface_area"], 
+                    moons=request_body["moons"],
+                    distance_from_sun=request_body["distance_from_sun"],
+                    namesake=request_body["namesake"])
 
-class Planet:
-    def __init__(self, id, name, description, moon):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.moon = moon
+    db.session.add(new_planet)
+    db.session.commit()
 
-PLANETS = [
-    Planet(1, "Mercury", "closest to the sun", 0),
-    Planet(2, "Venus", "very high temps", 0),
-    Planet(3, "Earth", "home", 1),
-    Planet(4, "Mars", "the red planet", 2),
-    Planet(5, "Jupiter", "famous spot", 80),
-    Planet(6, "Saturn", "famous rings", 83),
-    Planet(7, "Uranus", "spins upside down", 27),
-    Planet(8, "Neptune", "furthest from the sun", 14),
-]
+    return make_response(f"Planet {new_planet.name} successfully created", 201)
 
-planet_bp = Blueprint("planets", __name__, url_prefix="/planets")
-
-
-@planet_bp.route("", methods=["GET"])
-def get_all_planets():
-    planets_response = [vars(planet) for planet in PLANETS]
-    # for planet in PLANETS:
-    #     planets_response.append(
-    #         {
-    #             "id": planet.id,
-    #             "name": planet.name,
-    #             "description": planet.description,
-    #             "moon": planet.moon,
-    #         }
-    #     )
+@planets_bp.route("", methods=["GET"])
+def read_all_planets():
+    planets_response = []
+    planets = Planet.query.all()
+    for planet in planets:
+        planets_response.append(dict(
+            id=planet.id,
+            name=planet.name,
+            surface_area=planet.surface_area,
+            moons=planet.moons,
+            distance_from_sun=planet.distance_from_sun,
+            namesake=planet.namesake
+        ))
 
     return jsonify(planets_response)
-
-
-@planet_bp.route("/<planet_id>", methods=["GET"])
-def get_planet(planet_id):
-    planet = validate_planet(planet_id)
-    
-    return {
-        "id" : planet.id,
-        "name" : planet.name,
-        "description" : planet.description,
-        "moon" : planet.moon
-    }
-
-def validate_planet(planet_id):
-    try:
-        planet_id = int(planet_id)
-    except:
-        abort(make_response({"message":f"planet {planet_id} invalid"}, 400))
-
-    for planet in PLANETS:
-        if planet.id == planet_id:
-            return planet
-
-    abort(make_response({"message":f"planet {planet_id} not found"}, 404))
