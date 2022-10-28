d0cc987379fa21a40ef15dc99b0458c3da8a0c3a
Lindsey <lindseymarcea@icloud.com>
2022-10-28 18:56:39 -0400

    Create endpoints to post and get all books

--- app/routes.py
+++ app/routes.py
@@ -1,48 +1,48 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response, request
+from app import db
+from app.models.planet import Planet
 
-class Planet():
-    def __init__(self, id, name, description, color):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.color = color
+# class Planet():
+#     def __init__(self, id, name, description, color):
+#         self.id = id
+#         self.name = name
+#         self.description = description
+#         self.color = color
 
-PLANETS = [
-    Planet(1, "Saturn", "gassy giant", "orange"),
-    Planet(2, "Mercury", "small rocky", "gray"),
-    Planet(3, "Pluto", "still a planet", "white")
-]
+# PLANETS = [
+#     Planet(1, "Saturn", "gassy giant", "orange"),
+#     Planet(2, "Mercury", "small rocky", "gray"),
+#     Planet(3, "Pluto", "still a planet", "white")
+# ]
 
 planet_bp = Blueprint("Planet", __name__, url_prefix="/planet")
 
+@planet_bp.route("", methods=["POST"])
+def create_planet():
+    request_body = request.get_json()
+    new_planet = Planet(
+        name=request_body['name'], 
+        description=request_body['description'], 
+        color=request_body['color']
+    )
+    db.session.add(new_planet)
+    db.session.commit()
+
+    return make_response(f"Planet {new_planet.name} has been created", 201)
+
 @planet_bp.route("", methods=["GET"])
-def handle_books():
-    planets_response = []
-    for planet in PLANETS:
-        planets_response.append({
-            "id": planet.id,
-            "name": planet.name,
-            "description": planet.description,
-            "color": planet.color
-        })
-    return jsonify(planets_response)
+def get_all_planets():
+    results_list = []
+    all_planets = Planet.query.all()
 
-@planet_bp.route("/<id>", methods=["GET"])
-def get_one_planet(id):
-    try:
-        planet_id = int(id)
-    except:
-        return {"message":f"{id} is invalid"}, 400
-
-    for planet in PLANETS:
-        if planet.id == planet_id:
-            return {
-                "id": planet.id,
-                "name": planet.name,
-                "description": planet.description,
-                "color": planet.color
-            }, 200 
-    return {"message":f"{planet_id} not found"}, 404
+    for planet in all_planets:
+        results_list.append({
+        "name": planet.name,
+        "color": planet.color,
+        "description": planet.description,
+        "id": planet.id
+    })
+    return jsonify(results_list),200
 
 
 
