1cd0cb257bc11efa529481bc1f1152882232a75f
Erika Torres Figueroa <torrespurple@gmail.com>
2022-11-01 15:07:53 -0400

    re-added migrations folder, includes versions file

--- app/routes.py
+++ app/routes.py
@@ -5,39 +5,39 @@ from flask import Blueprint, jsonify, abort, make_response, request
 
 
 
-planet_list = [
-    Planet(1, "Mercury", "tiny and hot", 0.387),
-    Planet(2, "Venus", "big and hot", 0.722),
-    Planet(3, "Earth", "home", 1.0),
-]
+# planet_list = [
+#     Planet(1, "Mercury", "tiny and hot", 0.387),
+#     Planet(2, "Venus", "big and hot", 0.722),
+#     Planet(3, "Earth", "home", 1.0),
+# ]
 
 bp = Blueprint("planets", __name__, url_prefix="/planet")
 
 
-@bp.route("", methods=["GET"])
-def handle_planets():
-    planets_list = []
-    for planet in planet_list:
-        planets_list.append(planet.to_dict())
-    return jsonify(planets_list)
+# @bp.route("", methods=["GET"])
+# def handle_planets():
+#     planets_list = []
+#     for planet in planet_list:
+#         planets_list.append(planet.to_dict())
+#     return jsonify(planets_list)
 
 
-def validate_planet(planet_id):
-    try:
-        planet_id = int(planet_id)
-    except:
-        abort(make_response({"message": f"{planet_id} is not valid"}, 400))
+# def validate_planet(planet_id):
+#     try:
+#         planet_id = int(planet_id)
+#     except:
+#         abort(make_response({"message": f"{planet_id} is not valid"}, 400))
 
-    for planet in planet_list:
-        if planet.id == planet_id:
-            return jsonify(planet.to_dict())
-    abort(make_response({"message": f"{planet_id} is not found"}, 404))
+#     for planet in planet_list:
+#         if planet.id == planet_id:
+#             return jsonify(planet.to_dict())
+#     abort(make_response({"message": f"{planet_id} is not found"}, 404))
 
 
-@bp.route("/<planet_id>", methods=["GET"])
-def handle_planet(planet_id):
-    result_planet = validate_planet(planet_id)
-    return result_planet
+# @bp.route("/<planet_id>", methods=["GET"])
+# def handle_planet(planet_id):
+#     result_planet = validate_planet(planet_id)
+#     return result_planet
 
 
 @bp.route("", methods=["POST"])
