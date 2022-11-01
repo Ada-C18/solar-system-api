d7f8459bd8d5aa5ed1beee7d40cba0e049efb309
Elsje Slothower <elsje.slothower@gmail.com>
2022-11-01 10:55:59 -0700

    removed spaces in line 13

--- app/models/planet.py
+++ app/models/planet.py
@@ -10,18 +10,18 @@ class Planet(db.Model):
         return {
             "id": self.id,
             "name": self.name,
-            "distance from sun": self.distance_from_sun,
+            "distance_from_sun": self.distance_from_sun,
             "description": self.description
         }
 
-solar_system = [
-    Planet(1, "Mercury", 35000000, "smallest planet"),
-    Planet(2, "Venus", 67000000, "hottest planet"),
-    Planet(3, "Earth", 93000000, "only planet known to harbor intelligent life"),
-    Planet(4, "Mars", 142000000, "most likely planet to terraform"),
-    Planet(5, "Jupiter", 484000000, "largest planet"),
-    Planet(6, "Saturn", 889000000, "only planet with a ring system"),
-    Planet(7, "Uranus", 1790000000, "only planet with an almost vertical equator"),
-    Planet(8, "Neptune", 2880000000, "coldest planet"),
-    Planet(9, "Pluto", 3670000000, "only planet to be disowned fromt the Solar System")
-]
+# solar_system = [
+#     Planet(1, "Mercury", 35000000, "smallest planet"),
+#     Planet(2, "Venus", 67000000, "hottest planet"),
+#     Planet(3, "Earth", 93000000, "only planet known to harbor intelligent life"),
+#     Planet(4, "Mars", 142000000, "most likely planet to terraform"),
+#     Planet(5, "Jupiter", 484000000, "largest planet"),
+#     Planet(6, "Saturn", 889000000, "only planet with a ring system"),
+#     Planet(7, "Uranus", 1790000000, "only planet with an almost vertical equator"),
+#     Planet(8, "Neptune", 2880000000, "coldest planet"),
+#     Planet(9, "Pluto", 3670000000, "only planet to be disowned fromt the Solar System")
+# ]
