132f9979c2be9495fe7b44abbaef164e29d1b396
Lindsey <lindseymarcea@icloud.com>
2022-11-01 14:10:25 -0400

    Read one planet, update and delete planet  code added

--- app/routes.py
+++ app/routes.py
@@ -44,7 +44,53 @@ def get_all_planets():
     })
     return jsonify(results_list),200
 
+@planet_bp.route("/<planet_id>", methods=["GET"])
+def get_one_planet(planet_id):
+    planet = validate_planet(planet_id)
+    # planet = Planet.query.get(planet_id)
 
+    return {
+        "id": planet.id,
+        "name": planet.name,
+        "description": planet.description,
+        "color": planet.color
+    }
+
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except:
+        abort(make_response({"message":f"planet {planet_id} invalid"}, 400))
+
+    planet = Planet.query.get(planet_id)
+
+    if not planet:
+        abort(make_response({"message":f"planet {planet_id} not found"}, 404))
+
+    return planet
+
+@planet_bp.route("/<planet_id>", methods=["PUT"])
+def update_planet(planet_id):
+    planet = validate_planet(planet_id)
+
+    request_body = request.get_json()
+
+    planet.name = request_body["name"]
+    planet.description = request_body["description"]
+    planet.color = request_body["color"]
+
+    db.session.commit()
+
+    return make_response(f"Planet #{planet.id} successfully updated")
+
+@planet_bp.route("/<planet_id>", methods=["DELETE"])
+def delete_planet(planet_id):
+    planet = validate_planet(planet_id)
+
+    db.session.delete(planet)
+    db.session.commit()
+
+    return make_response(f"planet #{planet.id} successfully deleted")
 
 
 
