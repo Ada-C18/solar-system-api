2d95210ef98a15e599971cb771f33004f2f40993
Elsje Slothower <elsje.slothower@gmail.com>
2022-11-01 10:54:45 -0700

    updated db and migration changes within bp routes

--- app/routes.py
+++ app/routes.py
@@ -1,21 +1,11 @@
-from flask import Blueprint, jsonify, abort, make_response
+from os import abort
+from app import db
+from app.models.planet import Planet
+from flask import Blueprint, jsonify, abort, make_response, request
 
 solar_system_bp = Blueprint(
     "solar_system_bp", __name__, url_prefix="/solar-system"
     )
-
-@solar_system_bp.route("", methods=["GET"])
-def handle_planets():
-    planets_response = []
-    for planet in solar_system:
-        planets_response.append(planet.make_a_dict())
-    return jsonify(planets_response)
-
-@solar_system_bp.route("/<planet_id>", methods=["GET"])
-def handle_planet(planet_id):
-    planet = verify_planet(planet_id)
-
-    return jsonify(planet.make_a_dict())
 
 def verify_planet(planet_id):
     try:
@@ -23,11 +13,25 @@ def verify_planet(planet_id):
     except:
         abort(make_response({"message":f"planet {planet_id} invalid"}, 400))
     
-    for planet in solar_system:
-        if planet.id == planet_id:
-            return planet
+    planet = Planet.query.get(planet_id)
     
-    return abort(make_response(
+    if not planet:
+        abort(make_response(
         {"message":f"planet {planet_id} not found"}, 404
         ))
+    
+    return planet
+
+@solar_system_bp.route("", methods=["GET"])
+def read_all_planets():
+    planets_response = []
+    planets = Planet.query.all()
+    for planet in planets:
+        planets_response.append(planet.make_a_dict())
+    return jsonify(planets_response)
+
+@solar_system_bp.route("/<planet_id>", methods=["GET"])
+def read_one_planet(planet_id):
+    planet = verify_planet(planet_id)
+    return jsonify(planet.make_a_dict())
 
