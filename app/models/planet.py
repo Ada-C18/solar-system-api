98c25e08c3aede5cf288c996659a72f944dc8f33
ashanti <ammanier2@gmail.com>
2022-11-01 11:44:35 -0400

    changed planet model title to name

--- app/models/planet.py
+++ app/models/planet.py
@@ -2,6 +2,6 @@ from app import db
 
 class Planet(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    title = db.Column(db.String)
+    name = db.Column(db.String)
     description = db.Column(db.String)
     revolution_period = db.Column(db.String)
