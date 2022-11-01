dd064d5e4170e7601f07e692829f00bb67ca0f5f
Jamaica <jamaica@blackeverywhere.com>
2022-11-01 15:33:33 -0400

    Removed the hard coded class.  Our routes are working now.

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,91 @@
 # tell flask you want to import data
 from app import db 
 from app.models.planet import Planet
-from flask import Blueprint, jsonify, abort, make_response, request 
+from flask import Blueprint, jsonify, make_response, request, abort
 
-# create planet class
-class Planet:
+# create blueprint so the endpoint /planets can access planets data
+planets_bp = Blueprint('planets_bp', __name__, url_prefix='/planets')
 
-    def __init__(self, id, name, description, color):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.color = color
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+        
+    except:
+        abort(make_response({"message":f"planet {planet_id} invalid"}, 400))
+
+    planet = Planet.query.get(planet_id)
+    if not planet:
+        abort(make_response({"message":f"planet {planet_id} not found"}, 404))
+    return planet
+
+
+@planets_bp.route("", methods=["POST"])
+def create_planet():
+    request_body = request.get_json()
+    new_planet = Planet (
+                    name=request_body["name"],
+                    description=request_body["description"],
+                    color=request_body["color"])
+
+    db.session.add(new_planet)
+    db.session.commit()
+
+    return make_response(f"Planet {new_planet.name} successfully created", 201)
+
+
+@planets_bp.route("", methods=["GET"])
+def read_all_planets():
+    planets_response = []
+    planets = Planet.query.all()
+    for planet in planets:
+        planets_response.append(
+            {
+                "id": planet.id,
+                "name": planet.name,
+                "description": planet.description,
+                "color": planet.color
+            }
+        )
+    return jsonify(planets_response)
+
+@planets_bp.route("/<planet_id>", methods=["PUT"])
+def update_planet(planet_id):
+    planet = validate_planet(planet_id)
+
+    request_body = request.get_json()
+    
+    planet.id = request_body["id"]
+    planet.name = request_body["name"]
+    planet.description = request_body["description"]
+
+    db.session.commit()
+
+    return make_response(f"Planet {planet.title} successfully updated", 201)
+
+
+@planets_bp.route("/<planet_id>", methods=["DELETE"])
+def delete_planet(planet_id):
+    planet = validate_planet(planet_id)
+
+    db.session.delete(planet)
+    db.session.commit()
+
+    return make_response(f"Planet {planet.name} successfully deleted", 200)
+
+# # create planet class
+# class Planet:
+#     def __init__(self, id, name, description, color):
+#         self.id = id
+#         self.name = name
+#         self.description = description
+#         self.color = color
+
+# class Planet:
+#     def __init__(self, id, name, description, color):
+#         self.id = id
+#         self.name = name
+#         self.description = description
+#         self.color = color
 
 # create a list of 3 instances of planet class
 # should this also be a function?
@@ -23,10 +98,6 @@ class Planet:
 #     PLANET_2,
 #     PLANET_3
 # ]
-
-# create blueprint so the endpoint /planets can access planets data
-# do we need another blueprint for /planets/id?
-planets_bp = Blueprint('planets_bp', __name__, url_prefix='/planets')
 
 # validate planet
 # def validate_planet(planet_id):
@@ -47,17 +118,6 @@ planets_bp = Blueprint('planets_bp', __name__, url_prefix='/planets')
 #     except:
 #         abort(make_response({"message":f"planet {planet_id} not found"}, 404)) 
 
-@planets_bp.route("", methods=["POST"])
-def create_planet():
-    request_body = request.get_json()
-    new_planet = Planet(name=request_body["name"],
-                    description=request_body["description"],
-                    color=request_body["color"])
-
-    db.session.add(new_planet)
-    db.session.commit()
-
-    return make_response(f"Planet {new_planet.name} successfully created", 201)
 
 # allow get method for /planets/id (I think)
 # @planets_bp.route("/<planet_id>", methods=["GET"])
