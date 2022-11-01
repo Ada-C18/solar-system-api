6168ffe01c31a607095b963ab09b8c3ed1093209
Kae Sluder <kaesluder@fastmail.com>
2022-11-01 14:10:29 -0400

    modified routes to pgsql

--- app/routes.py
+++ app/routes.py
@@ -4,34 +4,40 @@ from flask import Blueprint, jsonify, abort, make_response, request
 
 
 
-
-planet_list = [
-    Planet(1, "Mercury", "tiny and hot", 0.387),
-    Planet(2, "Venus", "big and hot", 0.722),
-    Planet(3, "Earth", "home", 1.0),
-]
-
 bp = Blueprint("planets", __name__, url_prefix="/planet")
 
 
-@bp.route("", methods=["GET"])
-def handle_planets():
-    planets_list = []
-    for planet in planet_list:
-        planets_list.append(planet.to_dict())
-    return jsonify(planets_list)
+# @bp.route("", methods=["GET"])
+# def handle_planets():
+#     planets_list = []
+#     for planet in planet_list:
+#         planets_list.append(planet.to_dict())
+#     return jsonify(planets_list)
 
 
 def validate_planet(planet_id):
     try:
         planet_id = int(planet_id)
     except:
-        abort(make_response({"message": f"{planet_id} is not valid"}, 400))
+        abort(make_response({"message":f"planet {planet_id} invalid"}, 400))
 
-    for planet in planet_list:
-        if planet.id == planet_id:
-            return jsonify(planet.to_dict())
-    abort(make_response({"message": f"{planet_id} is not found"}, 404))
+    planet = Planet.query.get(planet_id)
+
+    if not planet:
+        abort(make_response({"message":f"planet {planet_id} not found"}, 404))
+
+    return planet
+
+# def validate_planet(planet_id):
+#     try:
+#         planet_id = int(planet_id)
+#     except:
+#         abort(make_response({"message": f"{planet_id} is not valid"}, 400))
+
+#     for planet in planet_list:
+#         if planet.id == planet_id:
+#             return jsonify(planet.to_dict())
+#     abort(make_response({"message": f"{planet_id} is not found"}, 404))
 
 
 @bp.route("/<planet_id>", methods=["GET"])
@@ -43,7 +49,7 @@ def handle_planet(planet_id):
 @bp.route("", methods=["POST"])
 def create_planet():
     request_body = request.get_json()
-    new_planet = Planet(id=request_body["id"],
+    new_planet = Planet(# id=request_body["id"],
                         name=request_body["name"],
                         description=request_body["description"],
                         distance=request_body["distance"])
