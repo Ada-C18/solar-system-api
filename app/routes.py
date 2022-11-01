58a5b9f3c4e82b70c45e1595ba0e6e520e9f857c
Irene <choireneh@gmail.com>
2022-11-01 13:37:34 -0400

    Added read, update, and delete methods

--- app/routes.py
+++ app/routes.py
@@ -11,13 +11,11 @@ def validate_planet(planet_id):
     except:
         abort(make_response({"message": f"planet {planet_id} invalid"}, 400))
 
-    planets = Planet.query.all()
-    for planet in planets:
-        if planet.id == planet_id:
-            return planet
+    planet = Planet.query.get(planet_id)
+    if not planet:
+        abort(make_response({"message": f"planet {planet_id} not found"}, 404))
 
-    abort(make_response({"message": f"planet {planet_id} not found"}, 404))
-
+    return planet
 
 @bp.route("", methods=["GET", "POST"])
 def handle_planets():
@@ -43,7 +41,26 @@ def handle_planets():
 
 
 @bp.route("/<planet_id>", methods=["GET"])
-def handle_planet(planet_id):
+def handle_one_planet(planet_id):
     planet = validate_planet(planet_id)
 
     return jsonify(planet.to_dict())
+
+@bp.route("/<planet_id>", methods=["PUT"])
+def update_planet(planet_id):
+    planet = validate_planet(planet_id)
+    request_body = request.get_json()
+
+    planet.name = request_body["name"]
+    planet.description = request_body["description"]
+    planet.rings = request_body["rings"]
+
+    db.session.commit()
+    return make_response(f"Planet #{planet.id} sucessfully updated"), 200
+
+@bp.route("/<planet_id>", methods=["DELETE"])
+def delete_planet(planet_id):
+    planet = validate_planet(planet_id)
+    db.session.delete(planet)
+    db.session.commit()
+    return make_response(f"Planet #{planet.id} successfully deleted"), 200
