22a7cae714f4e4410fb32eaf200e9f178d0e5a6f
Farrah <farrah.unger24@gmail.com>
2022-11-01 11:00:18 -0400

    pulling

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app(test_config=None):
     app = Flask(__name__)
 
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from app.models.planet import Planet
     # dot notation can mean they're in different levels
-    from .planets import planets_bp
+    from .planets import planets_bp   #(.routes)?
     app.register_blueprint(planets_bp)
 
     return app
