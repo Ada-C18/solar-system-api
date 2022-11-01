0103c59bc3a35017206d191eefa7fa209fa53d2c
Kallie Gannon <gannonkallie@gmail.com>
2022-11-01 10:59:49 -0400

    put, delete:single planet

--- app/planets.py
+++ app/planets.py
@@ -1,48 +1,104 @@
-from flask import Blueprint,jsonify,abort,make_response
+from flask import Blueprint,jsonify,abort,make_response,request
+from app import db
+from app.models.planet import Planet
 
 
+# class Planets:
+#     def __init__(self, id, name, color, description):
+#         self.id = id
+#         self.name = name
+#         self.color = color
+#         self.description = description
 
-class Planets:
-    def __init__(self, id, name, color, description):
-        self.id = id
-        self.name = name
-        self.color = color
-        self.description = description
-
-Planets_list = [
-    Planets(1, 'Dink', 'Green', 'Fluffy'), 
-    Planets(2, 'Blorp', 'Purple', 'Stinky'),
-    Planets(3, 'Florpus', 'Red', 'Shy')
-    ]
+# Planets_list = [
+#     Planets(1, 'Dink', 'Green', 'Fluffy'), 
+#     Planets(2, 'Blorp', 'Purple', 'Stinky'),
+#     Planets(3, 'Florpus', 'Red', 'Shy')
+#     ]
 
 planets_bp = Blueprint('planets_bp', __name__, url_prefix='/planets')
 
-@planets_bp.route('', methods=['GET'])
-def get_all_planets():
-    planet_response = [vars(planet) for planet in Planets_list]
+@planets_bp.route('', methods=['GET','POST'])
+def handle_planet():
+    if request.method == "GET":
+        planets = Planet.query.all()
+        planets_response = []
+        for planet in planets:
+            planets_response.append({
+                "id": planet.id,
+                "name": planet.name,
+                "color": planet.color,
+                "description": planet.description
+            })
+        return jsonify(planets_response)
+    elif request.method == "POST":
+        request_body = request.get_json()
+        new_planet = Planet(name=request_body["name"],
+                        color=request_body["color"],
+                        description=request_body["description"])
+    
+        db.session.add(new_planet)
+        db.session.commit()
 
-    return jsonify(planet_response)
 
-@planets_bp.route('/<id>', methods=['GET'])
-def get_one_planet(id):
-    # planet_id = int(id)
-    # for planet in Planets_list:
-    #     if planet.id == planet_id:
-    #         return vars(planet)
-    planet_id = validate_planet(id)
-    return planet_id
+        return make_response(f"Planet {new_planet.name} successfully created", 201)
 
-def validate_planet(id):
-    try:
-        planet_id = int(id)
-    except ValueError:
-        return {
-            "message": 'invalid id'
-        }, 400
 
-    for planet in Planets_list:
-        if planet.id == planet_id:
-            return vars(planet)
+@planets_bp.route("/<id>", methods=['GET','PUT','DELETE'])
+
+def handle_1_planet(id):
+    planets = Planet.query.get(id)
 
     
-    abort(make_response(jsonify(description ="Resource not found"),404))
+        if request.method == "GET":
+            return{
+                "id": planet.id,
+                "name": planet.name,
+                "color": planet.color,
+                "description": planet.description
+            }
+        elif request.method == "PUT":
+            request_body = request.get_json()
+            
+            planet.name = request_body["name"]
+            planet.color = request_body["color"]
+            planet.description = request_body["description"]
+
+            
+            db.session.commit()
+            return make_response(f"planet color {planet.color} succesfully updated",200)
+
+        elif request.method == "DELETE":
+            db.session.delete(planet)
+            db.session.commit()
+
+            return make_response(f"planet color {planet.color} succesfully deleted", 200)
+
+            
+
+
+
+# def get_all_planets():
+#     planet_response = [vars(planet) for planet in Planets_list]
+
+#     return jsonify(planet_response)
+
+# @planets_bp.route('/<id>', methods=['GET'])
+# def get_one_planet(id):
+#     planet_id = validate_planet(id)
+#     return planet_id
+
+# def validate_planet(id):
+#     try:
+#         planet_id = int(id)
+#     except ValueError:
+#         return {
+#             "message": 'invalid id'
+#         }, 400
+
+#     for planet in Planets_list:
+#         if planet.id == planet_id:
+#             return vars(planet)
+
+    
+#     abort(make_response(jsonify(description ="Resource not found"),404))
