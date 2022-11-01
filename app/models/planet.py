c89c328b2d9d98572994fcfb9b93f6ff124c8bae
Isabella <isabella.vanstedum@gmail.com>
2022-11-01 14:35:18 -0300

    did db migrate bc updated data type for name to String

--- app/models/planet.py
+++ app/models/planet.py
@@ -2,6 +2,6 @@ from app import db
 
 class Planet(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String, nullable=False)
     description = db.Column(db.String, nullable=False)
     radius = db.Column(db.Float, nullable=False)
