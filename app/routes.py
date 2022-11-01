37867e46666d7b7cffa97dd367e5655c8ca7ace2
Nicole Mejia <snicolemejia@gmail.com>
2022-11-01 10:51:25 -0400

    adds Planet model

--- app/routes.py
+++ app/routes.py
@@ -1,57 +1,57 @@
 from flask import Blueprint, jsonify, abort, make_response
 
 
-class Planet:
-    def __init__(self, id, name, description, num_moons):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.num_moons = num_moons
+# class Planet:
+#     def __init__(self, id, name, description, num_moons):
+#         self.id = id
+#         self.name = name
+#         self.description = description
+#         self.num_moons = num_moons
 
 
-PLANETS = [
-    Planet(1, 'Mercury', {"distance_from_sun": "36.04 million mi"}, 0),
-    Planet(2, 'Venus', {"distance_from_sun": "67.24 million mi"}, 0),
-    Planet(3, 'Earth', {"distance_from_sun": "92.96 million mi"}, 1),
-    Planet(4, 'Mars', {"distance_from_sun": "141.60 million mi"}, 2),
-    Planet(5, 'Jupiter', {"distance_from_sun": "483.80 million mi"}, 80),
-    Planet(6, 'Saturn', {"distance_from_sun": "890.8 million mi"}, 83),
-    Planet(7, 'Uranus', {"distance_from_sun": "1.784 billion mi"}, 27),
-    Planet(8, 'Neptune', {"distance_from_sun": "2.793 billion mi"}, 14),
-    Planet(9, 'Pluto', {"distance_from_sun": "3.70 billion mi"}, 5),
-]
+# PLANETS = [
+#     Planet(1, 'Mercury', {"distance_from_sun": "36.04 million mi"}, 0),
+#     Planet(2, 'Venus', {"distance_from_sun": "67.24 million mi"}, 0),
+#     Planet(3, 'Earth', {"distance_from_sun": "92.96 million mi"}, 1),
+#     Planet(4, 'Mars', {"distance_from_sun": "141.60 million mi"}, 2),
+#     Planet(5, 'Jupiter', {"distance_from_sun": "483.80 million mi"}, 80),
+#     Planet(6, 'Saturn', {"distance_from_sun": "890.8 million mi"}, 83),
+#     Planet(7, 'Uranus', {"distance_from_sun": "1.784 billion mi"}, 27),
+#     Planet(8, 'Neptune', {"distance_from_sun": "2.793 billion mi"}, 14),
+#     Planet(9, 'Pluto', {"distance_from_sun": "3.70 billion mi"}, 5),
+# ]
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
 
-@planets_bp.route("", methods=["GET"])
-def get_all_planets():
-    planets_response = [vars(planet) for planet in PLANETS]
-    # for planet in PLANETS:
-    #     planets_response.append({
-    #         "id": planet.id,
-    #         "name": planet.name,
-    #         "description": planet.description,
-    #         "number of moons": planet.num_moons
-    #     })
-    return jsonify(planets_response)
+# @planets_bp.route("", methods=["GET"])
+# def get_all_planets():
+#     planets_response = [vars(planet) for planet in PLANETS]
+#     # for planet in PLANETS:
+#     #     planets_response.append({
+#     #         "id": planet.id,
+#     #         "name": planet.name,
+#     #         "description": planet.description,
+#     #         "number of moons": planet.num_moons
+#     #     })
+#     return jsonify(planets_response)
 
 
-@planets_bp.route("/<id>", methods=["GET"])
-def get_one_planet(id):
-    planet = validate_planet(id)
-    return planet
+# @planets_bp.route("/<id>", methods=["GET"])
+# def get_one_planet(id):
+#     planet = validate_planet(id)
+#     return planet
 
 
-def validate_planet(id):
-    try:
-        planet_id = int(id)
+# def validate_planet(id):
+#     try:
+#         planet_id = int(id)
 
-    except ValueError:
-        return {"message": "planet does not exist"}, 400
+#     except ValueError:
+#         return {"message": "planet does not exist"}, 400
 
-    for planet in PLANETS:
-        if planet.id == planet_id:
-            return vars(planet)
+#     for planet in PLANETS:
+#         if planet.id == planet_id:
+#             return vars(planet)
     
-    abort(make_response(jsonify(description="resource not found"), 404))
+#     abort(make_response(jsonify(description="resource not found"), 404))
