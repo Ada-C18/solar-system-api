d2fe0db3090938eccab928f13b34b904c379c18c
ashanti <ammanier2@gmail.com>
2022-11-02 12:10:44 -0400

    refactored read_all_planets to accept/stack queries

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,9 +1,24 @@
+from os import abort
 from app import db
 from app.models.planet import Planet
 from flask import Blueprint, jsonify, abort, make_response, request
 
 planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
 
+#Error Handling an invalid planet or non-existing planet
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except:
+        abort(make_response({"message":f"planet {planet_id} invalid"}, 400))
+
+    planet = Planet.query.get(planet_id)
+
+    if not planet:
+        abort(make_response({"message":f"planet {planet_id} not found"}, 404))
+
+    return planet
+    
 #Create a new Planet
 @planets_bp.route("", methods=["POST"])
 def create_planet():
@@ -18,34 +33,53 @@ def create_planet():
 
     return make_response(f"Planet {new_planet.name} successfully created", 201)
 
+
+# @planets_bp.route("", methods=["GET"])
+# def read_all_planets():
+#     planets_response = []
+#     planets = Planet.query.all()
+#     for planet in planets:
+#         planets_response.append(
+#             {
+#                 "id": planet.id,
+#                 "name": planet.name,
+#                 "description": planet.description,
+#                 "revolution_period": planet.revolution_period
+#             }
+#         )
+#     return jsonify(planets_response)
+
+
+# --------------------- Refactoring read_all query to take query params ----------------------------
 @planets_bp.route("", methods=["GET"])
 def read_all_planets():
+    # this code replaces the previous (read_all_planets)query all code
+    # refactoring to stack queries
+
+    planet_query = Planet.query #use variable to stack on queries
+
+    name_query = request.args.get("name")
+    if name_query:
+        planet_query = planet_query.filter(Planet.name.ilike(f"%{name_query}%"))
+    
+    description_query = request.args.get("description")
+    if description_query:
+        planet_query = planet_query.filter(Planet.description.ilike(f"%{description_query}%"))
+    
+    planets = planet_query.all()
+    
+
     planets_response = []
-    planets = Planet.query.all()
     for planet in planets:
-        planets_response.append(
-            {
-                "id": planet.id,
-                "name": planet.name,
-                "description": planet.description,
-                "revolution_period": planet.revolution_period
-            }
-        )
+        planets_response.append({
+            "id": planet.id,
+            "name": planet.name,
+            "description": planet.description,
+            "revolution_period": planet.revolution_period
+        })
+
     return jsonify(planets_response)
 
-#Error Handling an invalid planet or non-existing planet
-def validate_planet(planet_id):
-    try:
-        planet_id = int(planet_id)
-    except:
-        abort(make_response({"message":f"planet {planet_id} invalid"}, 400))
-
-    planet = planet.query.get(planet_id)
-
-    if not planet:
-        abort(make_response({"message":f"planet {planet_id} not found"}, 404))
-
-    return planet
 
 #Get a Single(specific) planet endpoint
 @planets_bp.route("/<planet_id>", methods=["GET"])
@@ -58,6 +92,32 @@ def read_one_planet(planet_id):
         "description": planet.description,
         "revolution_period": planet.revolution_period
     }
+
+#Updating a Planet (endpoint)
+@planets_bp.route("/<planet_id>", methods=["PUT"])
+def update_planet(planet_id):
+    planet = validate_planet(planet_id)
+
+    request_body = request.get_json()
+
+    planet.name = request_body["name"]
+    planet.description = request_body["description"]
+    planet.revolution_period = request_body["revolution_period"]
+
+    db.session.commit()
+
+    return make_response(f"Planet #{planet.id} successfully updated")
+
+#Deleting a Planet (endpoint)
+@planets_bp.route("/<planet_id>", methods=["DELETE"])
+def delete_planet(planet_id):
+    planet = validate_planet(planet_id)
+
+    db.session.delete(planet)
+    db.session.commit()
+
+    return make_response(f"planet #{planet.id} successfully deleted")
+
 
 # class Planet:
 #     def __init__(self, id, name, description, revolution_period):
