3c992d0f576707f971665795641b9a8412f58636
Maria Silva <hello@mariasilva.dev>
2022-11-02 13:45:33 -0500

    passed wave 5

--- app/models/planet.py
+++ app/models/planet.py
@@ -5,10 +5,19 @@ class Planet(db.Model):
     name = db.Column(db.String)
     description = db.Column(db.String)
     moons = db.Column(db.Integer)
-        
-    def to_planet_dict(self):
-        return dict(
-            id=self.id, 
-            name=self.name, 
-            description=self.description,
-            moons=self.moons)
+
+
+    def to_dict(self):
+        planet_as_dict = {}
+        planet_as_dict["id"] = self.id
+        planet_as_dict["name"] = self.name
+        planet_as_dict["description"] = self.description
+
+        return planet_as_dict
+    
+    def from_json(cls, req_body):
+        return cls(
+            name = req_body["name"],
+            description = req_body["description"],
+            moons = req_body["moons"]
+        )
