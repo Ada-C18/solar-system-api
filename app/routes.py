1f5ad44ab37a3cc317c0f785b64f628fea84a05a
Cristal <cbcodee@gmail.com>
2022-11-02 15:52:22 -0700

    validate model function

--- app/routes.py
+++ app/routes.py
@@ -21,16 +21,35 @@ planet_bp = Blueprint("planets", __name__, url_prefix="/planets")
 def handle_planets():
     if request.method == "POST":
         request_body = request.get_json()
-        new_planet = Planet.from_dict(request_body)
+        new_planet = Planet(name=request_body["name"], 
+                            description=request_body["description"],
+                            flag=request_body["flag"])
         db.session.add(new_planet)
         db.session.commit()
         return make_response("yay!", 201)
-
     elif request.method == "GET":
-        planets = Planet.query.all() 
+        planets = Planet.query.all()
         planets_response = []
         planets_response.append([planet.to_dict() for planet in planets])
         return jsonify(planets_response)
+
+
+def validate_model(cls, model_id):
+    try:
+        model_id = int(model_id)
+    except:
+        abort(make_response({"message": f"object {model_id} is invalid"}, 400))
+    
+    model = cls.query.get(model_id)
+    if not model:
+        abort(make_response({"message": f"{model_id} not found"}, 404))
+
+
+@planet_bp.route("/<id>", methods=["GET"])
+def get_planet(id):
+    validate_model(Planet, id)
+    planet = Planet.query.get(id)
+    return jsonify(planet.to_dict())
 
 
 # @planet_bp.route("", methods=["GET"])
