c9c441817b11df95fc3c7cd549ca2312979623be
Kae Sluder <kaesluder@fastmail.com>
2022-11-01 20:05:23 -0400

    comment string to describe database conn

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ migrate = Migrate()
 def create_app(test_config=None):
     app = Flask(__name__)
 
+    # database = solar_system_development
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
 
