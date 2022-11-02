7bbf487ae77545e400fa9c771c427d4f746fe964
Maria Silva <hello@mariasilva.dev>
2022-11-01 23:03:20 -0500

    wave 4 completed

---
+++ app/models/planet.py
@@ -0,0 +1,14 @@
+from app import db
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String)
+    description = db.Column(db.String)
+    moons = db.Column(db.Integer)
+        
+    def to_planet_dict(self):
+        return dict(
+            id=self.id, 
+            name=self.name, 
+            description=self.description,
+            moons=self.moons)
--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,5 +1,5 @@
 from app import db
-from flask import Blueprint, jsonify, make_response, request
+from flask import Blueprint, jsonify, abort, make_response, request
 from app.models.planet import Planet
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
@@ -31,59 +31,55 @@ def read_all_planets():
             )
     return jsonify (planets_response), 200
 
-#"""""" #####learn to docstring
-# #OLD CODE BELOW
-# #created Planet class
-# class Planet:
-#     def __init__(self, id, name, description, moons):
-#         self.id = id
-#         self.name = name
-#         self.description = description
-#         self.moons = moons
+# GET ONE RESOURCE
+@planets_bp.route("/<id>", methods=["GET"])
+def get_one_planet(id):
+    planet = validate_planet(id)
 
+    return jsonify({
+    "id": planet.id, 
+    "name": planet.name, 
+    "description": planet.description,
+    "moons": planet.moons
+}), 200
 
-#     #method in class planet.to_planet
-#     def to_planet_dict(self):
-#         return dict(
-#             id=self.id, 
-#             name=self.name, 
-#             description=self.description,
-#             moons=self.moons
-#     )
-# #created planet instances
-# planets= [
-#     Planet(1, "fi", "red planet", 10),
-#     Planet(2, "fie", "blue planet", 9),
-#     Planet(3, "foo", "yellow planet", 8)
+# UPDATE RESOURCE
+@planets_bp.route("/<id>", methods=["PUT"])
+def update_planet(id):
+    planet = validate_planet(id)
+    request_body = request.get_json()
 
-# bp = Blueprint("planets", __name__, url_prefix="/planets") 
-# #call method .to planets, return jsonified result from dictionary
-# @bp.route("", methods=["GET"])
-# def handle_planets():
-#     results_list = []
-#     for planet in planets: 
-#                 results_list.append(planet.to_planet_dict())
-#     return jsonify(results_list)
+    planet.name = request_body["name"]
+    planet.description = request_body["description"]
+    planet.moons = request_body["moons"]
 
-# #validate input for request
-# def validate_planet(planet_id):
-#     #if string cannot cast to integer, return "bad request"
-#     try:
-#         planet_id= int(planet_id)
-#     except:
-#         abort(make_response({"message":f"planet {planet_id} invalid"}, 400))
+    db.session.commit()
 
-#     #if planet.id not found, return 404
-#     for planet in planets: 
-#         if planet.id == planet_id:
-#             return planet
+    return make_response(f"planet {id} successfully updated")
 
-#     abort(make_response({"message":f"planet {planet_id} not found"}, 404))
+# DELETE RESOURCE
+@planets_bp.route("/<id>", methods=["DELETE"])
+def delete_planet(id):
+    planet = validate_planet(id)
 
-# #create endpoint blueprint
-# @bp.route("/<id>", methods=["GET"])
-# def handle_planet(id):
-#     planet = validate_planet(id)
-#     return jsonify(planet.to_planet_dict())
+    db.session.delete(planet)
 
-#     """"""
+    db.session.commit()
+
+    return make_response(f"planet {id} successfully deleted")
+
+#validate input for request
+def validate_planet(planet_id):
+    #if string cannot cast to integer, return "bad request"
+    try:
+        planet_id= int(planet_id)
+    except:
+        abort(make_response({"message":f"planet {planet_id} invalid"}, 400))
+
+    #if planet.id not found, return 404
+    planet = Planet.query.get(planet_id)
+    if not planet:
+        abort(make_response({"message":f"planet {planet_id} not found"}, 404))
+
+    return planet
+    
