6c6ee1d6d56ee933bef483a8d98774ad1d3410ab
Rahel Kemal <rahelskemal@gmail.com>
2022-11-02 11:03:35 -0700

    added helper function

--- models/planet.py
+++ models/planet.py
@@ -5,3 +5,12 @@ class Planet(db.Model):
     name = db.Column(db.String)
     description = db.Column(db.String)
     moon_count = db.Column(db.Integer)
+
+    def to_dict(self):
+        planet_dict = {}
+        planet_dict["id"] = self.id
+        planet_dict["name"] = self.name
+        planet_dict["description"] = self.description
+        planet_dict["moon_count"] = self.moon_count
+
+        return planet_dict
