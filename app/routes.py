f24bc896aedc21d6eeace0ec8e698c77c49a0573
Mia Yang <miacxyang@gmail.com>
2022-11-02 11:22:05 -0700

    Part2 Wave 5

--- app/routes.py
+++ app/routes.py
@@ -52,15 +52,82 @@ from app.models.planet import Planet
 
     
 bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+def validate_model(cls, model_id):
+    try:
+        model_id = int(model_id)
+    except:
+        abort(make_response({"message": f"{cls.__name__} {model_id} invalid"}, 400))
+
+    model = cls.query.get(model_id)
+
+    if not model:
+        abort(make_response({"message": f"{cls.__name__} {model_id} not found"}, 404))
+
+    return model
+
 @bp.route("", methods=["POST"])
 def create_planet():
     request_body = request.get_json()
-    new_planet = Planet(name=request_body['name'], color=request_body['color'],description=request_body['description'])
+    new_planet = Planet.from_dict(request_body)
 
     db.session.add(new_planet)
     db.session.commit()
 
     return make_response(f"Planet {new_planet.name} successfully created", 201)
 
+@bp.route("", methods=["GET"])
+def read_all_planet():
+    name_query = request.args.get("name")
+    color_query = request.args.get("color")
+    description_query = request.args.get("description")
+    id_query = request.args.get("id")
+
+    planet_query = Planet.query
+
+    if name_query:
+        planet_query = planet_query.filter_by(name=name_query) 
+
+    if color_query:
+        planet_query = planet_query.filter_by(color=color_query)
+
+    if description_query:
+        planet_query = planet_query.filter_by(description = description_query)
+
+    if id_query:
+        planet_query = planet_query.filter_by(id= id_query)
+   
+    planets = planet_query.all()
+    
+    all_planets = [planet.to_dict() for planet in planets]
+
+    return jsonify(all_planets), 200
+
+@bp.route("/<id>", methods=["GET"])
+def read_one_planet(id):
+    planet = validate_model(Planet, id)
+    return jsonify(planet.to_dict()), 200
+
+@bp.route("/<id>", methods=["PUT"])
+def update_planet(id):
+    planet = validate_model(Planet, id)
+    request_body = request.get_json()
+
+    planet.name = request_body["name"]
+    planet.color = request_body["color"]
+    planet.description = request_body["description"]
+
+    db.session.commit()
+
+    return make_response(f"planet #{id} successfully updated"), 200
+
+@bp.route("/<id>", methods=["DELETE"])
+def delete_plante(id):
+    planet = validate_model(Planet, id)
+
+    db.session.delete(planet)
+    db.session.commit()
+
+    return make_response(f"planet #{id} successfully deleted"), 200 
 
 
