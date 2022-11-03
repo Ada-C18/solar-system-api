eef413f7685ecad83a60e0849477ded873d1ce42
Annie <annie.gallagher7@gmail.com>
2022-11-03 15:28:37 -0600

    updated validate_planet function to be broader for any model validation

--- app/models/planet.py
+++ app/models/planet.py
@@ -6,6 +6,13 @@ class Planet(db.Model):
     description = db.Column(db.String, nullable=False)
     moons = db.Column(db.Integer, nullable=False)
 
+    @classmethod
+    def from_dict(cls, planet_data):
+        new_planet = cls(name=planet_data["name"],
+                        description=planet_data["description"],
+                        moons=planet_data["moons"])
+        return new_planet
+
     def to_dict(self):
         return dict(
             id = self.id,
