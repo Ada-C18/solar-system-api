c8be2675aa21846a8b25ced90241718e3f0fda74
Marie Keefer <mkeefer17@gmail.com>
2022-11-03 14:55:45 -0400

    Created test to check GET /planets and returns 200 and empty array

---
+++ tests/test_routes.py
@@ -0,0 +1,9 @@
+# get all planets and return no records
+def test_get_all_planets_with_no_records(client):
+    # Act
+    response = client.get("/planets")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == []
