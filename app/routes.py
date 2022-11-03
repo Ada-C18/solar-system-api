3f298f92603242d7fe85b45414305de4d5f123a8
Bianca <biancadelascasas1@gmail.com>
2022-11-03 14:54:31 -0400

    removed comments

--- app/routes.py
+++ app/routes.py
@@ -68,35 +68,3 @@ def delete_planet(id):
     db.session.delete(planet)
     db.session.commit()
     return make_response(f'planet {planet.name}: sucessfully deleted', 200)
-
-
-# class Planet:
-#     def __init__(self, id, name, description, radius):
-#         self.id = id
-#         self.name = name
-#         self.description = description
-#         self.radius = radius
-#     def retrieve_planet(self):
-#         return dict(id=self.id, name=self.name, description=self.description, radius=self.radius)
-
-# planets = [
-#     Planet(1, 'Earth', 'good place', 6371 ), 
-#     Planet(2, 'Jupiter', 'fun place', 69901),
-#     Planet(3, 'Mars', 'weird place', 3389.5)
-# ]
-
-# bp = Blueprint('planets', __name__, url_prefix='/planets')
-# @bp.route('', methods=["GET"])
-# def get_planets():
-#     planet_data = []
-#     for planet in planets:
-#         planet_data.append(planet.retrieve_planet())
-#     return jsonify(planet_data)
-
-
-
-# @bp.route('<id>', methods=["GET"])
-# def get_planet(id):
-#     planet=validate_planet(id)
-#     return planet.retrieve_planet()
-    
