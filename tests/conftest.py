eb9cf22bd7253dc49d3965136aad2eb3db8d80db
n2020h <uzame.va@gmail.com>
2022-11-03 12:15:07 -0700

    get one book, test fails with  moons in assert statement

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ import pytest
 from app import create_app
 from app import db
 from flask.signals import request_finished
+from app.models.planet import Planet
 
 @pytest.fixture
 def app():
@@ -22,3 +23,12 @@ def app():
 @pytest.fixture
 def client(app):
     return app.test_client()
+
+@pytest.fixture
+def two_saved_planets(app):
+    # Arrange
+    red_planet = Planet(name="Mars", description="Too hot", moons = 2)
+    blue_planet = Planet(name="Earth",description="Home Sweet Home", moons = 1)
+
+    db.session.add_all([red_planet, blue_planet])
+    db.session.commit()
