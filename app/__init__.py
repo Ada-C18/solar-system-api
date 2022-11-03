1dbdd31a5949c8c8a21e5c45c7dcb5d4598ef91c
reneesu99 <reneesu22@gmail.com>
2022-11-03 10:52:37 -0700

    finished wave 6

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,33 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from dotenv import dotenv_values
+import os
 
 
 db = SQLAlchemy() # db and migrate are variables that gives us access to db operations
 migrate = Migrate()
+config = dotenv_values(".env")
+
+
+print(config["SQLALCHEMY_DATABASE_URI"])
 
 def create_app(test_config=None):
     app = Flask(__name__)
+    if not test_config:
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #hide a warning abt a feature that we wont be using
+        app.config['SQLALCHEMY_DATABASE_URI'] = config["SQLALCHEMY_DATABASE_URI"]
+    else:
+        app.config["TESTING"] = True
+        app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+        app.config["SQLALCHEMY_DATABASE_URI"] = config["SQLALCHEMY_TEST_DATABASE_URI"]
 
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #hide a warning abt a feature that we wont be using
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
-    
     # connects db & migrate to flask app
+    from app.models.planet import Planet
+
     db.init_app(app)
     migrate.init_app(app, db)
 
-    from app.models.planet import Planet
 
     from .routes import planet_bp
     app.register_blueprint(planet_bp)
---
+++ tests/test_routes.py
@@ -0,0 +1,102 @@
+from werkzeug.exceptions import HTTPException
+from app.routes import validate_id
+from app.models.planet import Planet
+import pytest
+
+
+# 1. `POST` `/planets` with a JSON request body returns a `201`
+
+def test_get_all_planets_empty(client):
+    # Act
+    response = client.get("/planets")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == []
+
+def test_get_planet_by_id(client, two_saved_planets):
+    response = client.get("/planets/1")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == {
+        "id": 1,
+        "name": "Mars",
+        "moons": 0,
+        "description": "no watr 4evr"
+    }
+
+def test_get_planet_by_id_empty(client):
+    response = client.get("/planets/1")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 404
+
+def test_get_all_planets(client, two_saved_planets):
+    # Act
+    response = client.get("/planets")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body[0] == {
+        "id": 1,
+        "name": "Mars",
+        "moons": 0,
+        "description": "no watr 4evr"
+    }
+    assert response_body[1] == {
+        "id": 2,
+        "name": "Jupiter",
+        "moons": 1,
+        "description": "i luv storms"
+    }
+
+def test_get_query_param_planets(client, two_saved_planets):
+    # Act
+    response = client.get("/planets?moons=0")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body[0] == {
+        "id": 1,
+        "name": "Mars",
+        "moons": 0,
+        "description": "no watr 4evr"
+    }
+
+
+
+def test_post_planet(client):
+    # Act
+    response = client.post("/planets", json={
+        "name": "Earth",
+        "description": "The Best!",
+        "moons": 1
+    })
+
+    # Assert
+    assert response.status_code == 201
+
+def test_update_planet(client, two_saved_planets):
+    # Act
+    response = client.put("/planets/1", json = {
+        "name":"Mars2",
+        "description": "The Best!",
+        "moons": 1
+    })
+    assert response.status_code == 200
+
+def test_delete_planet(client, two_saved_planets):
+    # Act
+    response = client.delete("/planets/1")
+    assert response.status_code == 200
+
+def test_delete_planet_invalid_id(client, two_saved_planets):
+    # Act
+    response = client.delete("/planets/random")
+    assert response.status_code == 400
