c56d26f0bc36b176aec4bf7f5c3d200b9cc5adc9
Reyna Diaz <reynamdiazz@gmail.com>
2022-11-03 14:27:16 -0400

    "fixture two_planets + test_by_id + id_not_found"

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from app import create_app
 from app import db
+from app.models.planet import Planet
 from flask.signals import request_finished
 
 
@@ -23,3 +24,13 @@ def app():
 @pytest.fixture
 def client(app):
     return app.test_client()
+
+@pytest.fixture
+def two_saved_planets(app):
+    big_planet = Planet(name="Jupiter",
+    description="big planet", moons = 80)
+    small_planet =  Planet(name="Mercury",
+    description="small planet", moons=0)
+
+    db.session.add_all([big_planet, small_planet])
+    db.session.commit()
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -4,3 +4,22 @@ def test_get_all_planets_with_no_records(client):
 
     assert response.status_code == 200
     assert response_body == []
+
+def test_get_planet_by_id(client, two_saved_planets):
+    response = client.get("planets/1")
+    response_body = response.get_json()
+
+    assert response.status_code == 200
+    assert response_body == {
+        "id":1,
+        "name": "Jupiter",
+        "description": "big planet",
+        "moons": 80
+    }
+
+def test_get_planet_by_id_not_found(client, two_saved_planets):
+    response = client.get("planets/6")
+    response_body = response.get_json()
+
+    assert response.status_code == 404
+    assert response_body == {"message":f"Planet 6 not found"}
