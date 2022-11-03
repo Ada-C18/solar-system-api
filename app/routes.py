bd658b2ba6031f709dcf7b89839194bc2856ca23
arika <arikaagnew@gmail.com>
2022-11-03 18:54:19 -0400

    "made minor change"

--- app/routes.py
+++ app/routes.py
@@ -2,18 +2,6 @@ from app import db
 from app.models.planet import Planet
 from flask import Blueprint, jsonify, make_response, request, abort
 
-
-# planets = [
-#     Planet(1,"Mercury","The smallest planet",1516),
-#     Planet(2, "Venus","The second planet from the Sun and Earth's closest planetary neighbor",3760.4),
-#     Planet(3, "Earth", "The third planet from the Sun and the only astronomical object known to harbor life",3958.8),
-#     Planet(4, "Mars","The fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury",2106.1),
-#     Planet(5, "Jupiter", "The fifth planet from the Sun and the largest in the Solar System",43441),
-#     Planet(6, "Saturn", "The sixth planet from the Sun and the second-largest in the Solar System, after Jupiter",36184),
-#     Planet(7, "Uranus", "The seventh planet from the Sun. Its name is a reference to the Greek god of the sky",15759),
-#     Planet(8, "Neptune", "The eighth planet from the Sun and the farthest known solar planet",15299)
-#     ]
-
 def validate_model(cls, model_id):
     try:
         model_id = int(model_id)
