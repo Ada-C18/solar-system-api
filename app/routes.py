7b8a16bb0d86615eeb27fc4e1afc2da4f49effe6
Annie <annie.gallagher7@gmail.com>
2022-11-03 15:28:57 -0600

    updated validate_planet function to be broader for any model validation

--- app/routes.py
+++ app/routes.py
@@ -13,9 +13,7 @@ planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 @planets_bp.route("", methods=["POST"])
 def create_planet():
     request_body = request.get_json()
-    new_planet = Planet(name=request_body["name"],
-                        description=request_body["description"],
-                        moons=request_body["moons"])
+    new_planet = Planet.from_dict(request_body)
     db.session.add(new_planet)
     db.session.commit()
     return make_response(jsonify(f"Planet {new_planet.name} has been created successfully"), 201)
@@ -41,13 +39,13 @@ def read_all_planets():
 
 @planets_bp.route("/<planet_id>", methods=["GET"])
 def planet_endpoint(planet_id):
-    planet = validate_planet(planet_id)
+    planet = validate_model(Planet, planet_id)
 
     return jsonify(planet.to_dict())
 
 @planets_bp.route("/<planet_id>", methods=["PUT"])
 def planet_update(planet_id):
-    planet = validate_planet(planet_id)
+    planet = validate_model(Planet, planet_id)
     request_body = request.get_json()
     planet.name = request_body["name"]
     planet.description = request_body["description"]
@@ -58,22 +56,22 @@ def planet_update(planet_id):
 
 @planets_bp.route("/<planet_id>", methods=["DELETE"])
 def planet_delete(planet_id):
-    planet = validate_planet(planet_id)
+    planet = validate_model(Planet, planet_id)
 
     db.session.delete(planet)
     db.session.commit()
 
     return make_response(jsonify(f"Planet {planet.name} has been deleted successfully"), 200)
 
-def validate_planet(planet_id):
+def validate_model(cls, model_id):
     try:
-        planet_id = int(planet_id)
+        model_id = int(model_id)
     except:
-        abort(make_response(jsonify({"message":f"planet {planet_id} invalid"}), 400))
+        abort(make_response(jsonify({"message":f"{cls.__name__} {model_id} invalid"}), 400))
     
-    planet = Planet.query.get(planet_id)
+    model = cls.query.get(model_id)
     
-    if not planet:
-        abort(make_response(jsonify({"message":f"planet {planet_id} not found"}), 404))
+    if not model:
+        abort(make_response(jsonify({"message":f"{cls.__name__} {model_id} not found"}), 404))
 
-    return planet
+    return model
