07fdcde0b73b7b45fae403f60ea475df36df2859
Elsje Slothower <elsje.slothower@gmail.com>
2022-11-03 11:14:50 -0700

    matched flasky's organization

--- app/models/planet.py
+++ app/models/planet.py
@@ -6,6 +6,12 @@ class Planet(db.Model):
     distance_from_sun = db.Column(db.BigInteger, nullable=False)
     description = db.Column(db.String, nullable=False)
 
+    @classmethod
+    def from_dict(cls, data_dict):
+        return cls(name=data_dict['name'], 
+            distance_from_sun=data_dict['distance_from_sun'],
+            description=data_dict['description'])
+
     def make_a_dict(self):
         return {
             "id": self.id,
@@ -13,23 +19,3 @@ class Planet(db.Model):
             "distance_from_sun": self.distance_from_sun,
             "description": self.description
         }
-
-    @classmethod
-    def from_dict(cls, planet_data):
-        new_planet = Planet(name=planet_data['name'], 
-                            distance_from_sun=planet_data['distance_from_sun'],
-                            description=planet_data['description'])
-
-        return new_planet
-
-# solar_system = [
-#     Planet(1, "Mercury", 35000000, "smallest planet"),
-#     Planet(2, "Venus", 67000000, "hottest planet"),
-#     Planet(3, "Earth", 93000000, "only planet known to harbor intelligent life"),
-#     Planet(4, "Mars", 142000000, "most likely planet to terraform"),
-#     Planet(5, "Jupiter", 484000000, "largest planet"),
-#     Planet(6, "Saturn", 889000000, "only planet with a ring system"),
-#     Planet(7, "Uranus", 1790000000, "only planet with an almost vertical equator"),
-#     Planet(8, "Neptune", 2880000000, "coldest planet"),
-#     Planet(9, "Pluto", 3670000000, "only planet to be disowned fromt the Solar System")
-# ]
