2c7d21968339a7b9c2aaf8903ad7b095b28e9e47
Elsje Slothower <elsje.slothower@gmail.com>
2022-11-03 14:30:30 -0700

    app.config in app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -9,8 +9,8 @@ def create_app(test_config=None):
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5000/solar_system_development'
     app.config['SQLALCHEMY_DATABASE_URI']="postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development"
+    app.config['SQLALCHEMY_TEST_DATABASE_URI']="postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_test"
 
     db.init_app(app)
     migrate.init_app(app, db)
