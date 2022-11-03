df19563f0960ba6b894e0ec8a0a34b33de8ccd38
Annie <annie.gallagher7@gmail.com>
2022-11-03 15:16:45 -0600

    removed unnecessary comments

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -9,11 +9,6 @@ def test_get_all_planets_with_no_records(client):
     assert response.status_code == 200
     assert response_body == []
 
-## Writing Tests
-
-# Create test fixtures and unit tests for the following test cases:
-
-# 1. `GET` `/planets/1` returns a response body that matches our fixture
 def test_get_one_planet(client, two_saved_planets):
     response = client.get("/planets/1")
     response_body = response.get_json()
@@ -26,7 +21,6 @@ def test_get_one_planet(client, two_saved_planets):
         "moons":0
     }
 
-# 1. `GET` `/planets/1` with no data in test database (no fixture) returns a `404`
 def test_get_one_planet_no_data(client):
     response = client.get("/planets/1")
     response_body = response.get_json()
@@ -34,7 +28,6 @@ def test_get_one_planet_no_data(client):
     assert response.status_code == 404
     assert response_body == {'message': 'planet 1 not found'}
 
-# 1. `GET` `/planets` with valid test data (fixtures) returns a `200` with an array including appropriate test data
 def test_get_planets(client, two_saved_planets):
     response = client.get("/planets/1")
     response_body = response.get_json()
@@ -53,7 +46,6 @@ def test_get_planets(client, two_saved_planets):
         "moons":0
     }
 
-# 1. `POST` `/planets` with a JSON request body returns a `201`
 def test_create_one_planet(client):
     response = client.post("/planets", json={
         "name": "Earth",
