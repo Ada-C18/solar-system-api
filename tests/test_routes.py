da835ed90b6f7b82306711e510e9b66381e326ec
galinaramaniuk <galina.sergarz@gmail.com>
2022-11-03 13:58:49 -0400

    created test_get_one_planet_returns_planet

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -6,3 +6,17 @@ def test_get_all_planets_with_no_records(client):
     # Assert
     assert response.status_code == 200
     assert response_body == []
+
+def test_get_one_planet_returns_planet(client, two_saved_planets):
+    # Act
+    response = client.get("/planets/1")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == {
+        "id": 1,
+        "name": "Mercury",
+        "description": "The smallest planet",
+        "radius": 1516
+    }
