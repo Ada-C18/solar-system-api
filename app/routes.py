fc56176406be75fbabd0aae2222fd19404652476
misha-joy <mishellajoy@gmail.com>
2022-11-03 11:18:54 -0700

    restoring to previous after merge conflict

--- app/routes.py
+++ app/routes.py
@@ -4,9 +4,44 @@ from flask import Blueprint, jsonify, abort, make_response, request
 
 planet_bp = Blueprint("planets", __name__, url_prefix = "/planets")
 
-# HELPER FUNCTIONS
+@planet_bp.route("", methods=["POST", "GET"])
+def handle_planets():
+    request_body = request.get_json()
+
+    planet_response = []
+
+    name_query = request.args.get("name")
+
+    if request.method == "GET":
+        planets = Planet.query.all()
+        planet_response = []
+        for planet in planets:
+            planet_response.append({
+                "id": planet.id,
+                "name": planet.name,
+                "color": planet.color,
+                "livability": planet.livability,
+                "moons": planet.moons,
+                "is_dwarf": planet.is_dwarf
+            })
+        return jsonify(planet_response)
+    
+    elif request.method == "POST": 
+        new_planet = Planet(
+        # id = request_body["id"],
+        name = request_body["name"],
+        color = request_body["color"],
+        moons = request_body["moons"],
+        livability = request_body["livability"],
+        is_dwarf = request_body["is_dwarf"]
+        )
+
+        db.session.add(new_planet)
+        db.session.commit()
+
+        return make_response(f"Planet {new_planet.name} successfully created", 201)
+
 def validate_planet_id(planet_id):
-    planet = validate_planet_id(planet_id)
     try:
         planet_id = int(planet_id)
     except:
@@ -16,49 +51,8 @@ def validate_planet_id(planet_id):
 
     if not planet:
         abort(make_response({"message":f"planet #{planet_id} doesn't exist."}, 404))
+
     return planet
-
-# MAIN FUNCTIONS HANDLING REQUESTS
-@planet_bp.route("", methods=["GET"])
-def get_planets():
-# planet = validate_planet_id(planet_id)
-    name_query = request.args.get("name")
-
-    if name_query is not None:
-        planets = Planet.query.filter_by(name=name_query)
-    else:
-        planets = Planet.query.all()
-    
-    planet_response = []
-    for planet in planets:
-        planet_response.append({
-            "id": planet.id,
-            "name": planet.name,
-            "color": planet.color,
-            "livability": planet.livability,
-            "moons": planet.moons,
-            "is_dwarf": planet.is_dwarf
-        })
-    return jsonify(planet_response)
-
-@planet_bp.route("", methods=["POST"])
-def create_new_planet():
-    planet = validate_planet_id(planet.id)
-
-    request_body = request.get_json()
-
-    new_planet = Planet(
-        name = request_body["name"],
-        color = request_body["color"],
-        moons = request_body["moons"],
-        livability = request_body["livability"],
-        is_dwarf = request_body["is_dwarf"]
-    )
-
-    db.session.add(new_planet)
-    db.session.commit()
-
-    return make_response(f"Planet {new_planet.name} successfully created", 201)
 
 @planet_bp.route("/<planet_id>", methods = ["GET"])
 def get_one_planet(planet_id):
@@ -72,7 +66,6 @@ def get_one_planet(planet_id):
             "moons": planet.moons,
             "is_dwarf": planet.is_dwarf
             }
-
 
 @planet_bp.route("/<planet_id>", methods = ["PUT"])
 def update_planet(planet_id):
