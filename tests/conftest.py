04e58f39d7ad479b3f4f9ccb03931861939f1055
Liat <liathirsh@gmail.com>
2022-11-04 10:47:22 -0400

    added get on planet test and created test fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -23,3 +23,27 @@ def app():
 @pytest.fixture
 def client(app):
     return app.test_client()
+
+@pytest.fixture
+def two_saved_planets(app):
+    #arrange
+    Saturn = Planet{
+        "id": 1,
+        "name": "Saturn",
+        "surface_area": 123890491,
+        "moons": 83,
+        "distance_from_sun": 123123123123,
+        "namesake": "The Roman god of time"
+    }
+    Mars = Planet{
+        "id": 2,
+        "name": "Mars",
+        "surface_area": 1010101,
+        "moons": 1,
+        "distance_from_sun": 123123123123,
+        "namesake": "The Roman god of time"
+    }
+
+    db.session.add_all([Saturn, Mars])
+    db.session.commit()
+
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -6,3 +6,20 @@ def test_get_all_planets_with_no_records(client):
     # Assert
     assert response.status_code == 200
     assert response_body == []
+
+
+def test_get_one_planet(client, two_saved_planets):
+    # Act
+    response = client.get("/planets/1")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == {
+        "id": 1,
+        "name": "Saturn",
+        "surface_area": 123890491,
+        "moons": 83,
+        "distance_from_sun": 123123123123,
+        "namesake": "The Roman god of time",
+    }
