d3d02f1b4e01478e79e9d382f3dc4e9ae3806cfc
Erika Torres Figueroa <torrespurple@gmail.com>
2022-11-03 20:37:08 -0400

    test routes for get one planet, get one planet no database, get all planets, and post one planet

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@ import pytest
 from app import create_app
 from app import db
 from flask.signals import request_finished
-
+from app.models.planet import Planet
 
 @pytest.fixture
 def app():
@@ -24,4 +24,15 @@ def app():
 def client(app):
     return app.test_client()
 
-    
+@pytest.fixture
+def one_saved_planet(app):
+    planet = Planet(name="Pluto", description="cold and icy", distance=15)
+    db.session.add(planet)
+    db.session.commit()
+    return planet
+
+@pytest.fixture
+def two_saved_planets(app):
+    planet1 = Planet(name="Pluto", description="cold and icy", distance=15)
+    planet2 = Planet(name="Venus", description="round and far away", distance= 42)
+    db.session.add_all([planet1, planet2])
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -8,3 +8,54 @@ def test_get_all_planets_with_no_records(client):
     # Assert
     assert response.status_code == 200
     assert response_body == []
+
+def test_get_one_planet_returns_planet(client, one_saved_planet):
+    response = client.get("/planet/1")
+    response_body = response.get_json()
+
+    # assert
+    assert response.status_code == 200
+    assert response_body["id"] == 1
+    assert response_body["name"] == "Pluto"
+    assert response_body["description"] == "cold and icy"
+    assert response_body["distance"] == 15
+
+def test_get_one_planet_missing_record(client):
+    response = client.get("/planet/1")
+    response_body = response.get_json()
+
+    # assert
+    assert response.status_code == 404
+    assert response_body == {"message":"Planet 1 not found"}
+   
+def test_get_planets_with_all_records(client, two_saved_planets):
+    response = client.get("/planet")
+    response_body = response.get_json()
+
+    # assert
+    assert response.status_code == 200
+    assert len(response_body) == 2
+    assert response_body[0] == {
+        "id": 1,
+        "name":"Pluto", 
+        "description":"cold and icy", 
+        "distance":15
+    }
+    assert response_body[1] == {
+        "id": 2,
+        "name":"Venus", 
+        "description":"round and far away", 
+        "distance": 42
+    }
+
+def test_create_one_planet(client):
+    response = client.post("/planet", json={
+        "name":"Venus", 
+        "description":"round and far away", 
+        "distance": 42
+    })
+    response_body = response.get_data(as_text=True)
+
+    # assert
+    assert response.status_code == 201
+    assert response_body == "Planet Venus successfully created"
