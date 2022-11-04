00298e297d6d56229bf28aa2537126751337bf62
Elsje Slothower <elsje.slothower@gmail.com>
2022-11-03 21:19:21 -0700

    created third test_create_planet_happy_path()

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,3 +1,5 @@
+from app.models.planet import Planet
+
 def test_get_all_planets_with_no_records(client):
     # act
     response = client.get("/solar-system")
@@ -9,7 +11,7 @@ def test_get_all_planets_with_no_records(client):
 
 def test_get_one_planet_returns_planet(client, one_saved_planet):
     # act
-    response = client.get("solar-system/1")
+    response = client.get("/solar-system/1")
     response_body = response.get_json()
 
     #assert
@@ -18,3 +20,23 @@ def test_get_one_planet_returns_planet(client, one_saved_planet):
     assert response_body["name"] == one_saved_planet.name
     assert response_body["distance_from_sun"] == one_saved_planet.distance_from_sun
     assert response_body["description"] == one_saved_planet.description
+
+def test_create_planet_happy_path(client):
+    # arrange
+    EXPECTED_PLANET = {
+        "name": "Earth",
+        "distance_from_sun": 93000000,
+        "description": "only planet known to harbor intelligent life"
+    }
+
+    # act
+    response = client.post("/solar-system", json=EXPECTED_PLANET)
+    response_body = response.get_data(as_text=True)
+    actual_planet = Planet.query.get(1)
+
+    # assert
+    assert response.status_code == 201
+    assert response_body == f"Planet {EXPECTED_PLANET['name']} successfully created"
+    assert actual_planet.name == EXPECTED_PLANET["name"]
+    assert actual_planet.distance_from_sun == EXPECTED_PLANET["distance_from_sun"]
+    assert actual_planet.description == EXPECTED_PLANET["description"]
