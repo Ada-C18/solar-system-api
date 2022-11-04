f7ccd27f25a7c3d5b51f68ef215cd4f5e813acd9
Yael <yaelechols@gmail.com>
2022-11-03 23:31:19 -0700

    Adds tests for moon_count query params

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -24,6 +24,28 @@ def test_get_all_planets_no_records(client):
     assert response.status_code == 200
     assert response_body == []
 
+def test_get_all_planets_with_moon_count_query_matching_one(client, two_saved_planets):
+    data = {'moon_count': 0}
+    response = client.get("/planets", query_string = data)
+    response_body = response.get_json()
+
+    assert response.status_code == 200
+    assert len(response_body) == 1
+    assert response_body[0] == {
+        "id": 2,
+        "name": "Neptune",
+        "description": "it may exist, it may not",
+        "moon_count": 0
+    }
+
+def test_get_all_planets_with_moon_count_query_matching_none(client, two_saved_planets):
+    data = {'moon_count': 671}
+    response = client.get("/planets", query_string = data)
+    response_body = response.get_json()
+
+    assert response.status_code == 200
+    assert response_body == []
+
 def test_get_planet_by_id(client, two_saved_planets):
     response = client.get("/planets/1")
     response_body = response.get_json()
