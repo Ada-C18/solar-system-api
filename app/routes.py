1161e4498748a3fb6206842f1620b31c1864c928
Liat <liathirsh@gmail.com>
2022-11-04 07:38:40 -0400

    fixed bugs in main

--- app/routes.py
+++ app/routes.py
@@ -4,21 +4,22 @@ from flask import Blueprint, jsonify, abort, make_response, request
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
-def validate_id(class_obj,id):
+
+def validate_id(class_obj, id):
     try:
         id = int(id)
     except:
-        abort(make_response({"message":f"{class_obj} {id} invalid"}, 400))
+        abort(make_response({"message": f"{class_obj} {id} invalid"}, 400))
 
     query_result = Planet.query.get(id)
 
     if not query_result:
-        abort(make_response({"message":f"{class_obj} {id} not found"}, 404))
+        abort(make_response({"message": f"{class_obj} {id} not found"}, 404))
 
     return query_result
-    
 
-@planets_bp.route("", methods=["POST"])        
+
+@planets_bp.route("", methods=["POST"])
 def add_planet():
     request_body = request.get_json()
     new_planet = Planet.create_from_json(request_body)
@@ -41,19 +42,19 @@ def read_all_planets():
         planets = Planet.query.all()
 
     planets_response = jsonify([planet.to_dict() for planet in planets])
-   
+
     return planets_response
 
 
 @planets_bp.route("/<planet_id>", methods=["GET"])
 def read_one_planet(planet_id):
-    planet = validate_id(Planet, id)
+    planet = validate_id(Planet, planet_id)
     return planet.to_dict()
 
 
 @planets_bp.route("/<planet_id>", methods=["PUT"])
 def update_planet(planet_id):
-    planet = validate_id(Planet, id)
+    planet = validate_id(Planet, planet_id)
 
     request_body = request.get_json()
 
@@ -70,7 +71,7 @@ def update_planet(planet_id):
 
 @planets_bp.route("/<planet_id>", methods=["DELETE"])
 def delete_one_planet(planet_id):
-    planet = validate_id(Planet, id)
+    planet = validate_id(Planet, planet_id)
 
     db.session.delete(planet)
     db.session.commit()
