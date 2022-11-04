240a0426492bc0a999e02a2714fa1aa96148032e
Elsje Slothower <elsje.slothower@gmail.com>
2022-11-03 20:25:47 -0700

    reconfigured create_app()

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
 
 db = SQLAlchemy()
 migrate = Migrate(compare_type=True)
@@ -9,9 +11,12 @@ def create_app(test_config=None):
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI']="postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development"
-    app.config['SQLALCHEMY_TEST_DATABASE_URI']="postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_test"
-
+    if not test_config:
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    else:
+        app.config["TESTING"] = True
+        app.config['SQLALCHEMY_TEST_DATABASE_URI'] = os.environ.get("SQLALCHEMY_TEST_DATABASE_URI")
+    
     db.init_app(app)
     migrate.init_app(app, db)
 
@@ -21,10 +26,3 @@ def create_app(test_config=None):
     from app.models.planet import Planet
 
     return app
-
-    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    # if not test_config:
-    #     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
-    # else:
-    #     app.config["TESTING"] = True
-    #     app.config['SQLALCHEMY_TEST_DATABASE_URI'] = os.environ.get("SQLALCHEMY_TEST_DATABASE_URI")
