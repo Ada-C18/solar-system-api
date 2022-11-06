53cca8adebd40a0168a9c63195dbe475e6b47416
Ihovanna <90429068+Ihovanna@users.noreply.github.com>
2022-11-05 20:35:38 -0700

    Migrated and upgraded model (my local database updated; you need to update your own database with upgrade commands

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,11 @@ from flask import Flask, Blueprint
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app(test_config=None):
     app = Flask(__name__)
-
-    db = SQLAlchemy()
-    migrate = Migrate()
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
@@ -16,7 +15,7 @@ def create_app(test_config=None):
     app.register_blueprint(planets_bp)
 
     db.init_app(app)
-    migrate,init_app(app, db)
+    migrate.init_app(app, db)
 
     from app.models.planet_model import Planet
 
--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,16 @@
 from flask import Blueprint, jsonify, make_response, abort
 
 planets_bp = Blueprint('planets_bp', __name__, url_prefix='/planets')
+
+
+'''
+WAVE 3!!
+Need to create endpoints with following functionality:
+    - 'POST' request to create new valid planet
+        * should receive a a success response
+    - 'GET' request to get all existing planets
+        * list should include
+            - planet id
+            - planet name
+            - planet description
+'''
