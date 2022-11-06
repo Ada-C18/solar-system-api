dc978e0db781468b8f80d15224c8e6f327f3cee1
Ihovanna <90429068+Ihovanna@users.noreply.github.com>
2022-11-05 20:16:56 -0700

    Created Planet model and initiated in app>__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,23 @@
 from flask import Flask, Blueprint
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 def create_app(test_config=None):
     app = Flask(__name__)
 
+    db = SQLAlchemy()
+    migrate = Migrate()
+
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
+
     from .routes import planets_bp
     app.register_blueprint(planets_bp)
+
+    db.init_app(app)
+    migrate,init_app(app, db)
+
+    from app.models.planet_model import Planet
 
     return app
