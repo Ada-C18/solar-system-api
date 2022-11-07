e541508d175210af65dcfb9986ac84fc220332a6
Ihovanna <90429068+Ihovanna@users.noreply.github.com>
2022-11-06 19:55:05 -0800

    Wave 3: HTTP POST requests updates local database with new data entried. HTTP GET request returns all entries in database.

--- app/__init__.py
+++ app/__init__.py
@@ -11,12 +11,12 @@ def create_app(test_config=None):
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
 
-    from .routes import planets_bp
-    app.register_blueprint(planets_bp)
-
     db.init_app(app)
     migrate.init_app(app, db)
 
     from app.models.planet_model import Planet
+
+    from .routes import planets_bp
+    app.register_blueprint(planets_bp)
 
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,32 @@
-from flask import Blueprint, jsonify, make_response, abort
+from app import db
+from flask import Blueprint, jsonify, make_response, abort, request
+from app.models.planet_model import Planet
 
 planets_bp = Blueprint('planets_bp', __name__, url_prefix='/planets')
 
+@planets_bp.route("", methods=["POST"])
+def create_planet():
+    request_body = request.get_json()
+    new_planet = Planet(
+        name=request_body["name"],
+        description=request_body["description"]
+        )
 
-'''
-WAVE 3!!
-Need to create endpoints with following functionality:
-    - 'POST' request to create new valid planet
-        * should receive a a success response
-    - 'GET' request to get all existing planets
-        * list should include
-            - planet id
-            - planet name
-            - planet description
-'''
+    db.session.add(new_planet)
+    db.session.commit()
+
+    return make_response(f"Planet {new_planet.name} has been added to the Planets database.", 201)
+
+@planets_bp.route("", methods=["GET"])
+def all_planets():
+    planets_response = []
+    planets = Planet.query.all()
+
+    for planet in planets:
+        planets_response.append({
+            "id": planet.id,
+            "name": planet.name,
+            "description": planet.description
+        })
+
+    return jsonify(planets_response)
