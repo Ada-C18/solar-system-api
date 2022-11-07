6ff90159552cb33aea51e5883e9e135ff08dc4ac
Nicole Mejia <snicolemejia@gmail.com>
2022-11-07 12:56:07 -0500

    added tests for to_dict

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -73,3 +73,65 @@ def test_post_planet_returns_201(client):
     assert new_planet.name == "Pluto"
     assert new_planet.description == "distance_from_sun: 3.70 billion mi"
     assert new_planet.num_moons == 5
+
+
+def test_get_all_planets_with_two_records(client, two_saved_planets):
+    response = client.get("/planets")
+    response_body = response.get_json()
+
+    assert response.status_code == 200
+    assert len(response_body) == 2
+    assert response_body[0] == {
+        "id": 1,
+        "name": "Mercury",
+        "description": "distance_from_sun: 36.04 million mi",
+        "number of moons": 0
+    }
+    assert response_body[1] == {
+        "id": 2,
+        "name": "Venus",
+        "description": "distance_from_sun: 67.24 million mi",
+        "number of moons": 0
+    }
+
+
+def test_get_all_planets_with_name_query_matching_none(client, two_saved_planets):
+    data = {'name': 'Anything'}
+    response = client.get("/planets", query_string = data)
+    response_body = response.get_json()
+
+    assert response.status_code == 200
+    assert response_body == []
+
+
+def test_get_all_planets_with_name_query_matching_one(client, two_saved_planets):
+    data = {'name': 'Mercury'}
+    response = client.get("/planets", query_string = data)
+    response_body = response.get_json()
+
+    assert response.status_code == 200
+    assert len(response_body) == 1
+    assert response_body[0] == {
+        "id": 1,
+        "name": "Mercury",
+        "description": "distance_from_sun: 36.04 million mi",
+        "number of moons": 0
+    }
+
+
+def test_get_one_planet_id_not_found(client, two_saved_planets):
+    # Act
+    response = client.get("/planets/3")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 404
+    assert response_body == {"message":"planet 3 not found"}
+
+
+def test_get_one_planet_id_invalid(client, two_saved_planets):
+    response = client.get("/planets/monkey")
+    response_body = response.get_json()
+
+    assert response.status_code == 400
+    assert response_body == {"message":"planet monkey invalid"}
